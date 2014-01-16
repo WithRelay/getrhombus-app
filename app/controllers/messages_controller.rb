a5f619626bb4f2d3f16ecdb48b6ec3a3d2433964
Taiwo <taiwo@taiwo-1215P.(none)>
2014-01-15 19:01:56 -0500

    1st deploy for rhombus

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,41 @@
+class MessagesController < ApplicationController
+
+	require 'uri'
+
+	def index
+	end
+ 
+	def send_text_message
+		url = URI.encode_www_form([["api_key", "0ed6ecb8"],
+					["api_secret", "b4f769d8"],
+					["from", "<redacted_phone_number>"],
+					["to", "<redacted_phone_number>"],
+					["text", "hello checking 2"],
+
+				])
+		@response = HTTParty.post('https://rest.nexmo.com/sms/json?'+ url, :headers => {"Content-Type" => "application/x-www-form-urlencoded"} )
+	end
+
+	def receive_text_message
+		if params[:text]
+			@message = Message.new
+			@message.text = params[:text]
+			@message.save
+			render status: 200
+		end
+	end
+
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def message_params
+      params.require(:message).permit(:text)
+    end
+
+
+end
