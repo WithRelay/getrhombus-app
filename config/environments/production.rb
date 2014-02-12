70f925c211ddbbed7ba82e8f97981375714d99cb
Taiwo Oyeniyi <muyiwaoyeniyi@yahoo.com>
2014-02-12 09:19:45 -0500

    asset compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rhombus::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
