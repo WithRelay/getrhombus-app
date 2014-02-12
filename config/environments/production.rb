4d5a42a120bc30179dbe01f9a546fc25d8ed6671
Taiwo Oyeniyi <muyiwaoyeniyi@yahoo.com>
2014-02-12 09:08:27 -0500

    asset compile true for cloud 66

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,9 @@ Rhombus::Application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true #false
+  config.assets.compile = false
+
+  config.assets.enabled = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
