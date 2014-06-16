089d41a4f94241b4ae972da5dc7ac5c839637088
Taiwo Oyeniyi <muyiwaoyeniyi@yahoo.com>
2014-06-16 19:44:48 -0400

    changed dashboard tile count

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -109,7 +109,7 @@ class MessagesController < ApplicationController
 				save_inbound_text(request.query_string, msg_code = 4)
 				@message = Message.new 				
 				@message.nexmo_send_text_message(14, params[:to], params[:msisdn], 
-					"To start using Rhombus, follow the link to complete your signup: https://www.getrhombus.com/signup?num=#{params[:msisdn]}")
+					"To start donating, follow the link to complete your signup: https://www.getrhombus.com/signup?num=#{params[:msisdn]}")
 			
 			else 	
 				
--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,7 @@ class User < ActiveRecord::Base
   def todays_stuff
     rhombus_number = self.rhombus_number          
     all_payments = self.transactions
-    todays_payments = all_payments.where(created_at: Time.zone.today)
+    todays_payments = all_payments.where("created_at >= ?", Time.zone.now.beginning_of_day)
     total_1, total_2 = 0, 0
     if self.user_level == 0      
       all_payments.each do |p|
