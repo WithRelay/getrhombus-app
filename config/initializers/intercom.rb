550a918f4be12488c5931472ca01f6db5bfc95e0
Taiwo Oyeniyi <muyiwaoyeniyi@yahoo.com>
2014-06-23 19:12:24 -0400

    show if users have card or stripe info

--- config/initializers/intercom.rb
+++ config/initializers/intercom.rb
@@ -37,11 +37,9 @@ IntercomRails.config do |config|
   # user object, or a Proc which will be passed the current user.
   #
   config.user.custom_data = {
-     #:user_level => Proc.new { |current_user| current_user.user_level },
-     :user_level => current_user.user_level,
-     :user_email => current_user.email#,
-     #:last_four => current_user.last_four,
-     #:stripe => current_user.provider#,
+     :user_level => Proc.new { |current_user| current_user.user_level },
+     :last_four => Proc.new { |current_user| current_user.last_four },
+     :stripe => Proc.new { |current_user| current_user.provider }#,
      #:plan => Proc.new { |current_user| current_user.plan.name },
      #:favorite_color => :favorite_color
   }
