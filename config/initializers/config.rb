634129bab74cb0160a6558ef3a3080c1bfbf21c3
Antonio Ruano Cuesta <antonio@fightmetric.com>
2015-04-17 19:43:56 -0400

    Specify pubnub's log location

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,8 +3,11 @@ config_file = File.join(Rails.root,'config','config.yml')
 raise "#{config_file} is missing!" unless File.exists? config_file
 CONFIG = YAML.load(ERB.new(File.read(config_file)).result)[Rails.env].symbolize_keys
 
+pubnub_logger = Logger.new(File.join(Rails.root, 'log', 'pubnub.log'))
+
 # Pubnub
 $pubnub = Pubnub.new(
   :publish_key   => Rails.application.secrets.pubnub["publish_key"],
-  :subscribe_key => Rails.application.secrets.pubnub["subscribe_key"]
+  :subscribe_key => Rails.application.secrets.pubnub["subscribe_key"],
+  :logger => pubnub_logger
 )
