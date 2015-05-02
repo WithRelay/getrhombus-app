1a1c214c7c30bfe1a757075eeb8c9c99670ca5c8
Taiwo <muyiwaoyeniyi@yahoo.com>
2015-05-02 18:06:07 -0400

    merchant profile form

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw  do
   get '/a' => 'static_pages#a'
   get '/b' => 'static_pages#b'
   get '/c' => 'static_pages#c'
+  get '/d' => 'static_pages#d'
 
   #match 'contact' => 'messages#new', :as => 'contact', :via => :get
   #match 'contact' => 'messages#create', :as => 'contact', :via => :post
