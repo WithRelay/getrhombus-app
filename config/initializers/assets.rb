917d66d62683e12dae3fcf377060f292883f23dc
Taiwo <muyiwaoyeniyi@yahoo.com>
2015-05-02 07:36:33 -0400

    started form validations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,8 @@ Rails.application.config.assets.paths << "#{Rails.root}/app/assets/html"      # 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( tokenize.js forms.css mainly_static_pages.css 500.html utils.js custom.js plugins.js bootstrap-select.js)       # added
+
+# added
+Rails.application.config.assets.precompile += %w( tokenize.js forms.css mainly_static_pages.css 500.html utils.js custom.js plugins.js bootstrap-select.js )
+Rails.application.config.assets.precompile += %w( formValidation.min.js bootstrap-formvalidator.min.js )
 Rails.application.config.assets.precompile += %w( application_dashboard_messaging.css application_dashboard_messaging.js )
