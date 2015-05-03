d9ae4fcdd49ec56bf48066fcd9732fca42327e68
Taiwo <muyiwaoyeniyi@yahoo.com>
2015-05-03 17:19:14 -0400

    added proxima nova font

--- app/assets/javascripts/dashboard/messaging_angularjs/custom_messaging.js
+++ app/assets/javascripts/dashboard/messaging_angularjs/custom_messaging.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {  
 
     // highlight chat box
-    $('.user-list').click(function() {
+    $('.user-list .item').click(function() {
         $('.user-list .item.chat_highlight').removeClass('chat_highlight'); 
         $(this).addClass('chat_highlight');   
     });
