6fd14c5387d33b27e350f5ff434e258679c0fca8
Elijah <elijah_george@hotmail.com>
2016-08-30 18:06:06 -0400

    RHOMBUSV1-32

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -30,8 +30,18 @@ class StaticPagesController < ApplicationController
 	def fb_webhook
 		#open tunnel: curl -Ls https://burrow.io/qzDWcjhe-lhp7HvZt | bash -s
 		#tunnel url: https://sqfosv4e.burrow.io -> http://localhost:3000
+
+		# verify_token: <facebook_webhook_verify_token> #use in verifying webhooks. Generated randomly by us
+
+		#access token below is the page access token unique to an app(Rhombus) an admin(Taiwo) and a FB page(shelflet)
+		#It is gotten as part the response to calling the /me/accounts?access_token (see below) using the access token gotten during
+		#fb authentication
 		# subscription request: curl -ik -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token="
-		# verify_token: <facebook_webhook_verify_token>
+
+
+		#access token below is the token given as part
+		#of credentials during after successful authentication if requesting permissions for  pages_show_list or manage_pages
+		# to get page access tokens for all the user's pages : https://graph.facebook.com/v2.6/me/accounts?access_token= 
 
 	    if params['hub.mode'] == 'subscribe' && params['hub.verify_token'] == "<facebook_webhook_verify_token>"
 	      render json: params['hub.challenge']
