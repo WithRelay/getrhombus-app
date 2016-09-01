c5b6934914a78dd0cd51e6185e2bee0a43b7b2af
Taiwo <taiwo@fightmetric.com>
2016-09-01 01:05:43 -0400

    RHOMBUSV1-2 build image logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def generate_image_thumbnails(images)
+    div = ""
+    images.each do |i|
+      div += "<div class='images'>" +
+        '<span class="delete-image" id="img_"' + i.id.to_s + '">X</span> <br>' +
+        '<img src="' + i.avatar.url + '" width="100" height="100" />' +
+        "<span>" + i.avatar_file_name + "</span>" +
+        "</div>"
+    end
+    div.html_safe
+  end
+
 end
