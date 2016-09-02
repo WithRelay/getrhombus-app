e5f83e8b12850b49855b90b19d3b5c4573916455
Taiwo <taiwo@fightmetric.com>
2016-09-02 16:29:50 -0400

    fix migration issue

--- db/migrate/20160902190929_add_indexes_to_columns.rb
+++ db/migrate/20160902190929_add_indexes_to_columns.rb
@@ -1,6 +1,6 @@
 class AddIndexesToColumns < ActiveRecord::Migration
   def change
-    #add_index :hashtags, :user_id
-    #add_foreign_key :hashtags, :users
+    add_index :hashtags, :user_id
+    add_foreign_key :hashtags, :users
   end
 end
