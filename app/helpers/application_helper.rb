bd79682ca260461f9f1bf42e0a8139ae78544c56
Taiwo <taiwo@fightmetric.com>
2016-09-01 21:46:01 -0400

    RHOMBUSV1-6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,14 @@ module ApplicationHelper
 
   def generate_image_thumbnails(images)
     div = ""
-    images.each do |i|
-      div += "<div class='images'>" +
-        '<span class="delete-image" id="img_"' + i.id.to_s + '">X</span> <br>' +
-        '<img src="' + i.avatar.url + '" width="100" height="100" />' +
-        "<span>" + i.avatar_file_name + "</span>" +
-        "</div>"
+    if images.present?
+      images.each do |i|
+        div += "<div class='images'>" +
+          '<span class="delete-image" id="img_"' + i.id.to_s + '">X</span> <br>' +
+          '<img src="' + i.avatar.url + '" width="100" height="100" />' +
+          "<span>" + i.avatar_file_name + "</span>" +
+          "</div>"
+      end
     end
     div.html_safe
   end
