c380446aeaad3554d4a066cf6613b0c4c040110a
Taiwo <taiwo@fightmetric.com>
2016-09-03 12:00:05 -0400

    RHOMBUSV1-84

--- app/jobs/my_job.rb
+++
@@ -1,8 +0,0 @@
-class MyJob
-  def self.perform
-    # if we get mysql has gone away errors
-    # ActiveRecord::Base.clear_active_connections!
-    # Do anything here
-    puts "Email user to complete their profile"
-  end
-end
--- config/initializers/webhooks.rb
+++
@@ -1,7 +0,0 @@
-=begin
-Rails.application.eager_load!
-
-MessageQuickly::Callback.subclasses.each do |callback|
-  MessageQuickly::CallbackRegistry.register(callback)
-end
-=end
