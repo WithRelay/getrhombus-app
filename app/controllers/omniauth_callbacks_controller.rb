748aa99e218c6a30462d06f01a025cc72765babf
bhishmaLamichhane <itbhishma@outlook.com>
2016-09-21 17:28:03 +0545

    Update facebook credential data model

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  
+  protect_from_forgery
   require 'pp'
 
   def twitter
@@ -17,22 +17,17 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def facebook
-    # if current_user && current_user.user_level == 1
-    #   if TwitterCred.from_omniauth(request.env["omniauth.auth"], current_user.id) == true
-    #     redirect_to user_path(current_user)
-    #     set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
-    #     return
-    #   end
-    #   redirect_to user_path(current_user), alert: "We were unable to connect your account to Facebook account. Please try again"
-    # else
-    #   redirect_to user_path(current_user), alert: "You cannot connect your Facebook account."
-    # end
-
-    pp request.env["omniauth.auth"]
-    redirect_to user_path(current_user)
+    if current_user && current_user.user_level == 1
+      if FbCred.from_omniauth(request.env["omniauth.auth"], current_user.id) == true
+        redirect_to user_path(current_user)
+        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+        return
+      end
+      redirect_to user_path(current_user), alert: "We were unable to connect your account to Facebook account. Please try again"
+    else
+      redirect_to user_path(current_user), alert: "You cannot connect your Facebook account."
+    end
   end
-
-
 
   def failure
     redirect_to user_path(current_user), alert: "We were unable to connect your account. Please try again"
--- app/models/fb_cred.rb
+++ app/models/fb_cred.rb
@@ -1,2 +1,21 @@
 class FbCred < ActiveRecord::Base
+
+  belongs_to :user
+
+  def self.from_omniauth(auth, id)
+    begin
+      where(id: id).first_or_initialize.tap do |user|
+        user.u_id = auth.uid
+        user.auth_token = auth.credentials.token
+        user.email = auth.info.email
+        user.name = auth.info.name        
+        user.image_url = auth.info.image
+        user.id = id
+        user.save
+      end
+      true
+    rescue StandardError => err
+      false
+    end
+  end
 end
