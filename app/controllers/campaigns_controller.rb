765c500661e84055679bf94a9b6feadff5904c76
Surya <surya@jyaasa.com>
2016-09-22 12:03:53 +0545

    Generate campaigns_controller with action new, index, edit, update and destroy Generate view edit, index and new for campaigns

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,18 @@
+# campaigns controller handles merchant's campagins with different channels like
+# MMS/SMS, email and facebook messeger
+class CampaignsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
