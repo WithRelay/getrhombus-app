22ac1c29a05e37b9030728d3a810786797b1aaa1
Taiwo Oyeniyi <taiwo@getrhombus.com>
2016-09-22 02:27:10 -0400

    minor changes

--- app/models/user_list.rb
+++ app/models/user_list.rb
@@ -3,7 +3,7 @@ class UserList < ActiveRecord::Base
   belongs_to :list
 
  def get_all_users_on_list(list_id)
-  	all_users = self.find_by(list_id:list)
+  	self.find_by(list_id: list_id)
  end
 
 end
