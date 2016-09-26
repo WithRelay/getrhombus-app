0c5e906a0c4a65d77cdb600e5b5b46804ca197fc
Surya <surya@jyaasa.com>
2016-09-26 17:14:52 +0545

    added a foreign key to campaign id to message table and fb_message table

---
+++ db/migrate/20160926065814_add_campaign_id_to_message_and_fb_message.rb
@@ -0,0 +1,9 @@
+# for adding campaign id to messages and fb message
+class AddCampaignIdToMessageAndFbMessage < ActiveRecord::Migration
+  def change
+    add_reference :messages, :campaign, index: true
+    add_reference :fb_messages, :campaign, index: true
+    add_foreign_key :messages, :campaigns
+    add_foreign_key :fb_messages, :campaigns
+  end
+end
