663a97833c218dbcab7d8a9e297b8eaeeb802814
Surya <surya@jyaasa.com>
2016-09-27 16:29:06 +0545

    added belongs to user relation in campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,11 +1,15 @@
 class Campaign < ActiveRecord::Base
   attr_accessor :list_id
   has_many :lists, through: :campaign_lists
-  has_many :campaign_lists
-  has_many :messages
+  has_many :campaign_lists, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  belongs_to :user
+  # enums for campaign
   enum channel: { sms: '0', mms: '1', facebook_messenger: '2', email: '3' }
   enum status: { active: 1, paused: 2, inactive: 3 }
   enum frequency_type: { one_time: '0', recurring: '1' }
   enum delivery_type: { now: '0', later: '1' }
   accepts_nested_attributes_for :messages
+  # validation of campaign attributes
+  validates_presence_of :channel, :repeat_days, :frequency_type
 end
