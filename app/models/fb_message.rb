7353a2467faa6afb64306b49fc757f8205f434c5
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-09-27 13:29:40 +0545

    make association for file attachments

--- app/models/fb_message.rb
+++ app/models/fb_message.rb
@@ -2,4 +2,7 @@ class FbMessage < ActiveRecord::Base
 	has_many :conversation_refs, as: :textable, dependent: :destroy
   has_many :conversations, through: :conversation_refs, dependent: :destroy
   belongs_to :fb_page
+  has_many :image_refs, as: :imageable, dependent: :destroy
+  has_many :images, through: :image_refs
+  validates :message_id, uniqueness: true
 end
