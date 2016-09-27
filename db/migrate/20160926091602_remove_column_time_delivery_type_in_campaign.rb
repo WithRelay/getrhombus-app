3209a59de4056b4befcfd95bff2e63ffbd355c00
Surya <surya@jyaasa.com>
2016-09-27 16:36:24 +0545

    migration generated for remove column and changing column type

--- db/migrate/20160926091602_remove_column_time_delivery_type_in_campaign.rb
+++ db/migrate/20160926091602_remove_column_time_delivery_type_in_campaign.rb
@@ -3,5 +3,6 @@ class RemoveColumnTimeDeliveryTypeInCampaign < ActiveRecord::Migration
     rename_column :campaigns, :date, :date_time
     remove_column :campaigns, :time, :datetime
     add_column :campaigns, :delivery_type, :string
+    change_column :campaigns, :frequency_type, :string
   end
 end
