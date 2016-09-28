ac80acd4f3e9061d6a4b5e50d7b841656eae284c
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-09-28 15:08:59 +0545

    Fix migration problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928073444) do
+ActiveRecord::Schema.define(version: 20160928091721) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street_address",   limit: 191
@@ -126,13 +126,12 @@ ActiveRecord::Schema.define(version: 20160928073444) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "auth_token", limit: 191
-    t.integer  "user_id",    limit: 4
     t.string   "time_zone",  limit: 191
     t.string   "gender",     limit: 191
+    t.integer  "user_id",    limit: 4
   end
 
   add_index "fb_creds", ["id"], name: "index_fb_creds_on_id", unique: true, using: :btree
-  add_index "fb_creds", ["user_id"], name: "index_fb_creds_on_user_id", unique: true, using: :btree
 
   create_table "fb_messages", force: :cascade do |t|
     t.text     "text",           limit: 65535
