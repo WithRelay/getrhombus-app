79b5e6727e3553c79247125709e4918155161225
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-09-28 16:55:16 +0545

    Hotfix

--- db/migrate/20160928094512_rename_customer_list_to_user_list.rb
+++ db/migrate/20160928094512_rename_customer_list_to_user_list.rb
@@ -1,11 +1,7 @@
 class RenameCustomerListToUserList < ActiveRecord::Migration
   def change
-    class RenameCustomerListToUserList < ActiveRecord::Migration
-      def change
-       rename_table :customer_lists, :user_lists
-       remove_index :user_lists, name: 'index_customer_lists_on_user_id'
-       add_index :user_lists, :user_id
-      end
-    end
+   rename_table :customer_lists, :user_lists
+   remove_index :user_lists, name: 'index_customer_lists_on_user_id'
+   add_index :user_lists, :user_id
   end
 end
