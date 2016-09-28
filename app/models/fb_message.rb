7e666ae1bd1ce9ab02a813490c6e70db0cc626d7
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-09-28 14:03:08 +0545

    Minor change

--- app/models/fb_message.rb
+++ app/models/fb_message.rb
@@ -1,8 +1,9 @@
 class FbMessage < ActiveRecord::Base
 	has_many :conversation_refs, as: :textable, dependent: :destroy
-  has_many :conversations, through: :conversation_refs, dependent: :destroy
+  has_many :conversations, through: :conversation_refs
   belongs_to :fb_page
+
   has_many :image_refs, as: :imageable, dependent: :destroy
-  has_many :images, through: :image_refs, dependent: :destroy
+  has_many :images, through: :image_refs
   validates :message_id, uniqueness: true
 end
