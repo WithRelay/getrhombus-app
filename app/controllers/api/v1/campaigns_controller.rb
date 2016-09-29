fb0c98a841b4bbef105edaa268e7b53440f45ac2
Surya <surya@jyaasa.com>
2016-09-29 16:59:07 +0545

    created a function upload_images for handling editor picture upload

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -4,4 +4,16 @@ class Api::V1::CampaignsController < API::V1::BaseController
     image_ref.delete if image_ref
     render json: { response: "Deleted" }, status: 200
 	end
+
+  def upload_images
+    image = Image.new(avatar: params[:image])
+    if image.save
+      render json: {
+        href: image.avatar.url, type: image.avatar_content_type.split('/').first,
+        name: image.avatar_file_name
+      }
+    else
+      render json: { status: 401, message: 'sorry file could not upload' }
+    end
+  end
 end
