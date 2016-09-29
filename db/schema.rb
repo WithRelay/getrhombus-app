1c29d7067e8c6965604c6218985172349cad86b9
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-09-29 17:12:42 +0545

    add col to conversation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929042740) do
+ActiveRecord::Schema.define(version: 20160929091326) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street_address",   limit: 191
@@ -99,6 +99,8 @@ ActiveRecord::Schema.define(version: 20160929042740) do
     t.integer  "merchant_id", limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "page_id",     limit: 4
+    t.boolean  "resolution",  limit: 1
   end
 
   create_table "coupons", force: :cascade do |t|
