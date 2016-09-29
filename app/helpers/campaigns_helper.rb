09316b3511657dc3f178d3520a432010d2c0af8c
Surya <surya@jyaasa.com>
2016-09-29 16:55:24 +0545

    created a function is_one_time_checked and is_recurring_checked for checked the campaign value

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,6 +1,6 @@
 module CampaignsHelper
   def channel_list(campaign)
-    channel_hash = { 'SMS'=> 0, 'MMS'=> 1, 'Facebook_Messenger'=> 2, 'email'=> 3 }
+    channel_hash = { 'SMS'=> 0, 'MMS'=> 1, 'Facebook_Messenger'=> 2, 'Email'=> 3 }
     channel = campaign.channel
     if channel.present?
       channel_selected = { channel => channel_hash[channel] }
@@ -9,5 +9,18 @@ module CampaignsHelper
     else
       channel_hash
     end
+  end
+
+  def is_one_time_checked?(campaign)
+    camapaign_frquency = campaign.frequency_type
+    if camapaign_frquency.present?
+      camapaign_frquency == 'one_time'
+    else
+      return true
+    end
+  end
+
+  def is_recurring_checked?(campaign)
+    campaign.frequency_type == 'recurring'
   end
 end
