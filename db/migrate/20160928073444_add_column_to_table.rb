eb507e149bf2f46244ce412e9b32a2be43413699
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-10-03 11:53:57 +0545

    remove duplicate migration line

--- db/migrate/20160928073444_add_column_to_table.rb
+++ db/migrate/20160928073444_add_column_to_table.rb
@@ -1,6 +1,5 @@
 class AddColumnToTable < ActiveRecord::Migration
   def change
-    remove_column :fb_creds, :image_url
     add_column :fb_creds, :time_zone, :string
     add_column :fb_creds, :gender, :string
     add_column :conversations, :created_at, :datetime
