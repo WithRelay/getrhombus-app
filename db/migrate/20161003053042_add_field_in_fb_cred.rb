292eaee98e846b8990726176a9150132187134f2
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-10-03 11:54:51 +0545

    add fields in fb_cred

--- db/migrate/20161003053042_add_field_in_fb_cred.rb
+++ db/migrate/20161003053042_add_field_in_fb_cred.rb
@@ -1,12 +1,10 @@
 class AddFieldInFbCred < ActiveRecord::Migration
   def up
-    add_column :fb_creds, :profile_pic_url , :text
-    add_column :fb_creds, :fb_id, :string
-    add_column :fb_creds, :page_specific_id, :string
+    # add_column :fb_creds, :profile_pic_url , :text
+    # add_column :fb_creds, :page_specific_id, :string
   end
 
   def down     
-    remove_column :fb_creds, :u_id  
     remove_column :fb_creds, :page_specific_id, :string
     remove_column :fb_creds, :profile_pic_url
   end
