3959e0aaf8093334f72f9fe8f637005e77633bd6
Surya <surya@getrhombus.com>
2016-10-06 13:15:38 +0545

    added validation for text

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,12 +4,13 @@ class Campaign < ActiveRecord::Base
   has_many :campaign_lists, dependent: :destroy
   has_many :messages, dependent: :destroy
   belongs_to :user
+  has_many :image_refs, as: :imageable, dependent: :destroy
+  has_many :images, through: :image_refs
   # enums for campaign's class attributes channel, status, frequency_type and delivery_type
   enum channel: { sms: 0, mms: 1, facebook_messenger: 2, email: 3 }
   enum status: { active: 1, paused: 2, inactive: 3 }
   enum frequency_type: { one_time: 0, recurring: 1 }
   enum delivery_type: { later: 0, now: 1 }
-  accepts_nested_attributes_for :messages, reject_if: proc { |message| message[:text].blank? }
   # validation of campaign attributes
-  validates_presence_of :channel, :repeat_days, :frequency_type
+  validates_presence_of :channel, :repeat_days, :frequency_type, :text
 end
