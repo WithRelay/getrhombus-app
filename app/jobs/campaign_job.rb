180657a9b5911a063b63212f65d5ba99e600aa39
Surya <surya@getrhombus.com>
2016-10-06 21:55:28 +0545

    create a hash to send to mandrill in job

--- app/jobs/campaign_job.rb
+++ app/jobs/campaign_job.rb
@@ -6,7 +6,13 @@ class CampaignJob < ActiveJob::Base
     campaign = Campaign.find_by_id(campaign_id)
     if campaign.present?
       campaign.update_attributes(status: 3)
-      email_list = Campaign.first.lists.map{|list| {email: list.user.email } if list.user.present? }
+      email_list = campaign.lists.map{ |list| {email: list.user.email } if list.user.present? }
+      image_params = campaign.images.map{ |image|  if image.avatar.present?
+                                                    { type: image.avatar_content_type,
+                                                      name: image.avatar_file_name,
+                                                      content: Base64.encode64(open(image.avatar.url) { |image| image.read }) }
+                                                    end
+                                                    }
       EmailingService.send_email_campaign({ html: campaign.text, to: email_list }) if email_list.present?
     end
   end
