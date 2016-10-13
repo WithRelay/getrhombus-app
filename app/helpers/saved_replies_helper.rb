067f50da544832cf51b357663c553eaf636082b7
Taiwo Oyeniyi <taiwo@getrhombus.com>
2016-10-13 19:36:16 +0100

    RHOMBUSV1-74 remove helper method for saved replies

--- app/helpers/saved_replies_helper.rb
+++ app/helpers/saved_replies_helper.rb
@@ -1,10 +1,2 @@
 module SavedRepliesHelper
-def saved_replies_list
-    saved_replies = current_user.saved_replies
-    replies_hash = {}
-    saved_replies.each do |reply|
-      replies_hash.store(reply.title, reply.body)
-    end
-    replies_hash
-  end
 end
