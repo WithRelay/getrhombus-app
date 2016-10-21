1cc67ae623b06a683448d76aca5dd0ae34aed575
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-10-21 15:08:59 +0545

    ui fix on fb_page view

--- app/assets/javascripts/fb_page.js
+++ app/assets/javascripts/fb_page.js
@@ -35,10 +35,10 @@
 function check_status(val){
   if (val === 'false'){
     $('.submit_page').val('Subscribe');
-    $('.submit_page').attr('class', 'btn btn-success');
+    $('.submit_page').attr('class', 'submit_page btn btn-success');
   }
   else{
     $('.submit_page').val('Unsubscribe');
-    $('.submit_page').attr('class', 'btn btn-primary');
+    $('.submit_page').attr('class', 'submit_page btn btn-warning');
   }
 }
