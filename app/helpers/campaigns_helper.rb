4a74edc20e64d6df4ea316e21ec4300eee98ebce
Surya <surya@getrhombus.com>
2016-10-21 15:32:04 +0545

    added repeat every days option in drop down menu

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,5 +1,5 @@
 module CampaignsHelper
-  
+
   def channel_list(campaign)
     # Switching between channels is probably dangerous for persisted campaigns
     # Ex: Email content can't become sms
@@ -7,7 +7,7 @@ module CampaignsHelper
       { campaign.channel => get_channel_enum_value(campaign) }
     else
       channels = Campaign.channels
-      channels.delete('MMS') if !current_user.can_send_mms?   # Twilio MMS support only in US, CA  
+      channels.delete('MMS') if !current_user.can_send_mms?   # Twilio MMS support only in US, CA
       channels
     end
   end
@@ -25,7 +25,9 @@ module CampaignsHelper
     campaign.frequency_type == 'recurring'
   end
 
-  def repeat_days_options 
-    { '7 days' => 7, '14 days' => 14, '30 days' => 30, '60 days' => 60, '90 days' => 90 } 
+  def repeat_days_options
+    { 'Repeat every'=>'repeat everyday', '7 days' => 7, '14 days' => 14, '30 days' => 30,
+      '60 days' => 60, '90 days' => 90
+    }
   end
 end
