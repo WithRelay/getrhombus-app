cfb976942ff2cc8b3e4b48f27e43c8694149b3b6
Surya <surya@getrhombus.com>
2016-10-21 22:45:47 +0545

    set active campaign to run

--- app/jobs/channel_job.rb
+++ app/jobs/channel_job.rb
@@ -2,7 +2,7 @@ class ChannelJob
   @queue = :send_email
 
   def self.perform
-    Campaign.recurring.each do |campaign|
+    Campaign.recurring.active.each do |campaign|
       utc_date_time = campaign.date_time.in_time_zone(campaign.user.time_zone).utc
       CampaignJob.set(wait_until: utc_date_time).perform_later(campaign.id)
     end if campaign.present?
