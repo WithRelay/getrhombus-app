54266536d13d5ab80377ac8085b07b664d8e6cca
Surya <surya@getrhombus.com>
2016-10-21 15:34:51 +0545

    use active job for send now campaign

--- app/services/campaign_service.rb
+++ app/services/campaign_service.rb
@@ -1,21 +1,11 @@
 # services that schedule email sending for campaign
 class CampaignService < ScheduleService
 
-  def set_background_jobs
-    schedule_now_with_date_time(DateTime.now) if check_now?
+  def send_now
+    schedule_now
   end
 
-  private
-
-  def check_now?
-    (is_one_time_now? || is_recurring_with_now?)
-  end
-
-  def is_one_time_now?
-    @object.one_time? && @object.now?
-  end
-
-  def is_recurring_with_now?
-    @object.recurring? && @object.now?
+  def schedule_in_background
+    @job.set(wait_until: @object.date_time).perform_later(@object.id)
   end
 end
