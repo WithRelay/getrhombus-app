092f69b6860bfcae196b368a1b539b18bfc9fef1
Surya <surya@getrhombus.com>
2016-10-25 16:22:23 +0545

    creates a function channel_hash which returns campaign channel class name

--- app/jobs/campaign_job.rb
+++ app/jobs/campaign_job.rb
@@ -3,7 +3,17 @@ class CampaignJob < ActiveJob::Base
   queue_as :default
 
   def perform(campaign)
-    channel_class = "#{campaign.channel.split(" ").join()}Service".constantize
+    channel_class = channel_hash[campaign.channel].constantize
     channel_class.send_campaign(campaign)
+  end
+
+  def channel_hash
+    # this hash is because no need to use lots of conditional statement, if you have a
+    # differenct class associate with channel please provide the appropriate class name
+    {
+      "sms"=>"SmsService", "mms"=>"MmsService",
+      "facebook_messenger"=>"FacebookMessengerService",
+      "email"=>"EmailingService"
+    }
   end
 end
