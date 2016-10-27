f087b3267dfad06902a47969a11f9728b66eaa0c
Surya <surya@getrhombus.com>
2016-10-27 17:16:31 +0545

    create a private class method completed_campaign_present? for checking campaign should be send for rescue or not

--- app/jobs/send_campaign_job.rb
+++ app/jobs/send_campaign_job.rb
@@ -2,10 +2,19 @@ class SendCampaignJob
   @queue = :send_campaigns
 
   def self.perform
-    recurring_active_campaign = Campaign.recurring.active
-    recurring_active_campaign.each do |campaign|
+    campaigns = Campaign.recurring.active
+    campaigns.each do |campaign|
       utc_date_time = campaign.date_time.in_time_zone(campaign.user.time_zone).utc
-      Resque.enqueue_at_with_queue('default', utc_date_time, ChannelJob, campaign.id)
-    end if recurring_active_campaign.present?
+      Resque.enqueue_at_with_queue('default',
+                                    utc_date_time,
+                                    ChannelJob,
+                                    campaign.id) if uncompleted_campaign_present?(campaign)
+    end if campaigns.present?
+  end
+
+  private_class_method
+
+  def self.uncompleted_campaign_present?(campaign)
+    campaign.repeat_days == 0 ? true : campaign.repeat_days != campaign.send_count
   end
 end
