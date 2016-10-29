2bf00a5f868c9ae2bc1af68fb6855e6675468539
Taiwo Oyeniyi <taiwo@getrhombus.com>
2016-10-29 15:52:54 -0400

    update rake task for people model

--- lib/tasks/move_some_user_data_to_people_table.rb
+++ lib/tasks/move_some_user_data_to_people_table.rb
@@ -5,32 +5,32 @@
 =begin
   def change
     remove_column :users, :first_name
-	remove_column :users, :last_name
+    remove_column :users, :last_name
   end
 
  # some cleanup
- # null or nothing	
-	select * from users where user_level = 0 
-	and last_name = '' # first_name = '' # is null
+ # null or nothing  
+    select * from users where user_level = 0 
+    and last_name = '' # first_name = '' # is null
 
-	select * from users where lower(card_name) = 'Visa'
+    select * from users where lower(card_name) = 'Visa'
 
-	select * from users where card_name = card_type
+    select * from users where card_name = card_type
 
-	select * from users where user_level = 1 
-	and last_name is null and first_name is not null
+    select * from users where user_level = 1 
+    and last_name is null and first_name is not null
 
 =end
 
 
-desc "move some user data to people table"
+desc "Move merchant person data to people table"
 task :move_some_user_data_to_people_table => :environment do
 
-  User.all.each do |u|
+  User.where(user_level: 1).each do |u|
 
-  	# move only merchants even if some user info is incomplete
-  	if u.user_level == 1 && (u.first_name.present? || u.last_name.present?)    	
-    	Person.create(first_name: u.first_name, last_name: u.last_name, role: 'representative', user_id: u.id)
-	end
+    # move only merchants even if some user info is incomplete
+    if (u.first_name.present? || u.last_name.present?)      
+      Person.create(first_name: u.first_name, last_name: u.last_name, role: 'representative', user_id: u.id)
+    end
   end
 end
