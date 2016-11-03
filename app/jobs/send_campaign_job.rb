2703d7f37c3b5662be97358b6007e69d84d58409
Surya <surya@getrhombus.com>
2016-11-03 13:28:03 +0545

    send campaign which are of todays date

--- app/jobs/send_campaign_job.rb
+++ app/jobs/send_campaign_job.rb
@@ -4,17 +4,24 @@ class SendCampaignJob
   def self.perform
     campaigns = Campaign.recurring.active.includes([:images, lists:[:user_lists]])
     campaigns.each do |campaign|
-      utc_date_time = campaign.date_time.in_time_zone(campaign.user.time_zone).utc
+      user_date_time = campaign.date_time.in_time_zone(campaign.user.time_zone)
+      sending_time = Time.parse(user_date_time.strftime("%I:%M%p")).utc
       Resque.enqueue_at_with_queue('default',
-                                    utc_date_time,
+                                    sending_time,
                                     ChannelJob,
-                                    campaign.id) if uncompleted_campaign_present?(campaign)
-    end if campaigns.present?
+                                    campaign.id) if uncompleted_campaign_present?(campaign) && date_campaign(utc_date_time)
+    end
   end
 
   private_class_method
 
   def self.uncompleted_campaign_present?(campaign)
     campaign.repeat_days == 0 ? true : campaign.repeat_days != campaign.send_count
+  end
+
+  def self.date_campaign(date)
+    campaign_date = date.strftime("%Y-%m-%d")
+    date_today = Time.now.utc.strftime("%Y-%m-%d")
+    date_campaign == date_today
   end
 end
