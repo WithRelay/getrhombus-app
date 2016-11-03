07e62457d14ddd34dc69f2077cea40de36586815
Surya <surya@getrhombus.com>
2016-11-03 12:10:35 +0545

    while updating delete enque jobs and reschedule jobs with new date time

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -10,7 +10,7 @@ class CampaignsController < ApplicationController
   end
 
   def new
-    @campaign = Campaign.new
+    @campaign = current_user.campaigns.new
   end
 
   def create
@@ -18,9 +18,7 @@ class CampaignsController < ApplicationController
     campaign_params[:list_ids].split(',').each { |list_id| @campaign.campaign_lists.build(list_id: list_id) }
     save_campaign_images(@campaign)
     if @campaign.save
-      utc_date_time = @campaign.date_time.in_time_zone(@campaign.user.time_zone).utc
-      Resque.enqueue_at_with_queue('default', utc_date_time, ChannelJob, @campaign.id) if is_campaign_date_selected?(@campaign)
-      CampaignJob.perform_now(@campaign) if @campaign.deliver_now
+      enqueue_jobs(@campaign)
       flash[:notice] = 'Campaign Saved successfully'
       redirect_to new_user_campaign_path
     else
@@ -39,7 +37,8 @@ class CampaignsController < ApplicationController
     campaign_params[:list_ids].split(',').each { |list_id| @campaign.campaign_lists.build(list_id: list_id).save }
     save_campaign_images(@campaign)
     if @campaign.update_attributes(campaign_params)
-      change_campaign_job
+      destroy_campaign_jobs
+      enqueue_jobs(@campaign)
       flash[:notice] = 'Campaign updated successfully'
     else
       flash[:error] = @campaign.errors.messages
@@ -78,7 +77,7 @@ class CampaignsController < ApplicationController
   def change_campaign_job
     date_today = Date.today.strftime("%Y-%m-%d")
     utc_date_time = @campaign.date_time.in_time_zone(@campaign.user.time_zone).utc
-    today_campaign = utc_date_time.strftime("%Y/%m/%d") == date_today
+    today_campaign = utc_date_time.strftime("%Y-%m-%d") == date_today
     if @campaign.active? && today_campaign
       Resque.enqueue_at_with_queue('default', utc_date_time, ChannelJob, @campaign.id)
     else
@@ -103,6 +102,12 @@ class CampaignsController < ApplicationController
 
   def is_campaign_date_selected?(campaign)
     (campaign.one_time? && !campaign.deliver_now?)
+  end
+
+  def enqueue_jobs(campaign)
+    utc_date_time = campaign.date_time.in_time_zone(campaign.user.time_zone).utc
+    Resque.enqueue_at_with_queue('default', utc_date_time, ChannelJob, campaign.id) if is_campaign_date_selected?(campaign)
+    CampaignJob.perform_now(campaign) if campaign.deliver_now
   end
 
   def save_campaign_images(campaign)
