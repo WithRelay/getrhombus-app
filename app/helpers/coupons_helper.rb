e42e867e07d45beb1a408838f6da628ba57e4326
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-11-12 10:08:27 +0545

    show coupon reedem_by time in user timezone

--- app/helpers/coupons_helper.rb
+++ app/helpers/coupons_helper.rb
@@ -3,4 +3,9 @@ module CouponsHelper
   def get_selected_coupon_type(coupon)
     coupon.new_record? || coupon.amount_off.present? ? "amount_off" : "percent_off"
   end
+
+  def zone_timestamp(timestamp)
+    # Format the string output with #strftime method => "March 23, 2013 at 09:48 AM"
+    Time.at(timestamp).in_time_zone(current_user.time_zone).strftime("%B %e, %Y at %I:%M %p")
+  end
 end
