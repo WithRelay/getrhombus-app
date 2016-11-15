708b5eddcb2932cf7e49ad2eb50993cf4e805eda
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-11-15 09:29:12 +0545

    check active coupon on coupon presenter

--- app/presenters/subscription_presenter.rb
+++ app/presenters/subscription_presenter.rb
@@ -8,18 +8,22 @@ class SubscriptionPresenter < BasePresenter
   end
 
   def get_coupon_info
-    coupon = Coupon.find @model.coupon_id
-    coupon_name = coupon.name
-    discount = "#{coupon.currency} " + "%.2f" %(coupon.amount_off.to_f/100)
-    percent_off = "#{coupon.percent_off}%"
-    discount = percent_off if coupon.percent_off
-    if coupon.duration == 'repeating'
-      interval = (coupon.duration_in_months > 1) ? ("months") : "month"
-      duration = "for #{coupon.duration_in_months} #{interval}"
+    if @model.coupon_id
+      coupon = Coupon.find @model.coupon_id
+      coupon_name = coupon.name
+      discount = "#{coupon.currency} " + "%.2f" %(coupon.amount_off.to_f/100)
+      percent_off = "#{coupon.percent_off}%"
+      discount = percent_off if coupon.percent_off
+      if coupon.duration == 'repeating'
+        interval = (coupon.duration_in_months > 1) ? ("months") : "month"
+        duration = "for #{coupon.duration_in_months} #{interval}"
+      else
+        duration = coupon.duration
+      end
+      "#{coupon_name}: #{discount} #{duration}"
     else
-      duration = coupon.duration
+      "No active coupon"
     end
-    "#{coupon_name}: #{discount} #{duration}"
   end
 
 end
