af76bd72c568bc9e66effc8a854b05800803baec
Surya <surya@getrhombus.com>
2016-11-24 16:26:47 +0545

    RHOMBUSV1-102 added serialize fields_needed column of table stripe_creds

--- app/models/stripe_cred.rb
+++ app/models/stripe_cred.rb
@@ -1,6 +1,8 @@
 class StripeCred < ActiveRecord::Base
 
+  # for saving array in fields_needed column http://api.rubyonrails.org/classes/ActiveRecord/Base.html#M001799
+  serialize :fields_needed
   belongs_to :user
   enum uid_type: [ :managed, :standalone ]
-  
+
 end
