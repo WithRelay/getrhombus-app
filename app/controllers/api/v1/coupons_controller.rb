bcf95aa3149fa9101d642cdf2c21144829c660de
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-11-25 10:20:19 +0545

    RHOMBUSV1-85 move get_coupon and get_plan in their respective api controllers

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -8,4 +8,18 @@ class Api::V1::CouponsController < API::V1::BaseController
       render json: { valid: false }.to_json
     end
   end
+
+  def get_coupon
+    begin
+      if params[:name]
+        res = current_user.coupons.where("lower(name) like ?", "%#{params[:name].downcase}%")
+      else
+        res = current_user.coupons
+      end
+      render json: { "coupons" => res }, status: 200
+    rescue StandardError => e
+      render json: { error: "Unable to find your Coupons" }, status: 500
+    end
+  end
+
 end
