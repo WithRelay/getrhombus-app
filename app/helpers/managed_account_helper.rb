e4ee782484b778532fa8c3dced21aea81b02aebf
Surya <surya@getrhombus.com>
2016-11-29 16:42:45 +0545

    RHOMBUSV1-102 created helper method url_for_create_update for dynamic url for manged_account

---
+++ app/helpers/managed_account_helper.rb
@@ -0,0 +1,10 @@
+module ManagedAccountHelper
+
+  def url_for_create_update
+    if current_user.stripe_creds.present?
+      'update_managed_acct'
+    else
+      'create_managed_acct'
+    end
+  end
+end
