076a221757bf397a0d98e6c625318aab5dc6689c
Surya <surya@getrhombus.com>
2016-11-30 16:42:54 +0545

    RHOMBUSV1-102 check user has managed account or not and create url according to it

--- app/helpers/managed_account_helper.rb
+++ app/helpers/managed_account_helper.rb
@@ -1,7 +1,7 @@
 module ManagedAccountHelper
 
   def url_for_create_update
-    if current_user.stripe_creds.present?
+    if current_user.stripe_creds.managed.present?
       'update_managed_acct'
     else
       'create_managed_acct'
