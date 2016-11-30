ce8d83a62da506cfd67c450b3233d045b850fcca
Surya <surya@getrhombus.com>
2016-11-30 12:57:29 +0545

    write migration for rollback

--- db/migrate/20161130063535_add_gender_to_peoples.rb
+++ db/migrate/20161130063535_add_gender_to_peoples.rb
@@ -1,5 +1,9 @@
 class AddGenderToPeoples < ActiveRecord::Migration
-  def change
+  def up
     add_column :people, :gender, :string
+  end
+
+  def down
+    remove_column :people, :gender, :string
   end
 end
