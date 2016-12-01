3387f6ae08452dfdeda813b48aece632005e68ce
Surya <surya@getrhombus.com>
2016-12-01 15:58:52 +0545

    RHOMBUSV1-102 create helper methods for not allowing user to change business type and country

--- app/helpers/managed_account_helper.rb
+++ app/helpers/managed_account_helper.rb
@@ -7,4 +7,22 @@ module ManagedAccountHelper
       'create_managed_acct'
     end
   end
+
+  def connect_org_type(account)
+    org_type = { 'Individual' => 'Individual', 'Organization' => 'company' }
+    if account.org_type.present?
+      selected_org_type = {}
+      user_org_type = org_type.key(account.org_type)
+      selected_org_type[user_org_type] = account.org_type
+      selected_org_type
+    else
+      org_type
+    end
+  end
+
+  def connect_country(user)
+    country_list = PaymentService.stripe_country_list.collect{ |k,v| [v[0], k]}
+    selected_country = country_list.select{ |country| country.include?(user.country.to_sym) }
+    selected_country[0].present? ? selected_country : country_list
+  end
 end
