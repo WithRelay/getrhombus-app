318649aa72574b80ad61b1158d47236a5212f4b3
Taiwo Oyeniyi <taiwo@getrhombus.com>
2016-12-08 01:21:27 -0500

    minor changes

--- app/models/twilio_number_data.rb
+++ app/models/twilio_number_data.rb
@@ -1,7 +1,7 @@
 class TwilioNumberData < ActiveRecord::Base
 
   # save or update data in db
-  def self.add_or_update_opencnam_data(phone, city, state, zip, country)
+  def self.add_or_update_twilio_number_data(phone, city, state, zip, country)
     where(phone_number: phone).first_or_initialize.tap do |row|
       row.city = city
       row.state = state
