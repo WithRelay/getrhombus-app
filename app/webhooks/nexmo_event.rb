61463ecd60d7094b1ca77af377fcc516b8081a61
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-12-13 12:41:28 +0545

    RHOMBUSV1-109 refactor nexmo event for outbound message

--- app/webhooks/nexmo_event.rb
+++ app/webhooks/nexmo_event.rb
@@ -2,44 +2,41 @@ class NexmoEvent
 
   class << self
 
-     def process_event(params)
-      @param = params
-      save_message
-     end
+   def process_event(params)
+    @param = params
+    save_message if @param[:messageId]
+   end
 
-     # save both inbound messages and outbound events
-     def save_message
-      @message = Message.where(message_id: @param['messageId']).first_or_initialize
-      @message.update(
-        to: @param['to'],
-        from: @param['msisdn'],
-        message_timestamp: @param['message-timestamp'],
-        message_price: @param['price'],
-        status: @param['status'],
-        error_code: @param['err-code'],
-        user_id: get_user_id,
-        user_id_to: get_merchant_id,
-        text: @param['text']
-      )
+   # save both inbound and outbound messages events
+   def save_message
+    @message = Message.where(message_id: @param[:messageId]).first_or_initialize
+    update_message
+   end
 
-      # outbound events
-      # text 
+  def update_message
+    @message.to = @param[:to]
+    @message.from = @param[:msisdn]
+    @message.message_timestamp = @param['message-timestamp']
+    @message.user_id = get_user_id
+    @message.user_id_to = get_merchant_id
+    @message.text ||= @param[:text]
+    @message.message_price = @param[:price]
+    @message.status = @param[:status]
+    @message.error_code = @param['err-code']
+    @message.save
+  end
 
-      # inbound messages
-      # status, err-code, price 
-     end
+   private
 
-     private
+   def get_user_id
+     user = User.find_by(phone_number:  @param[:to])
+     user.id if user
+   end
 
-     def get_user_id
-       user = User.find_by(phone_number:  @param['to'])
-       user.id if user
-     end
-
-     def get_merchant_id
-       merchant = User.find_by(rhombus_number: @param['msisdn'])
-       merchant.id if merchant
-     end
+   def get_merchant_id
+     merchant = User.find_by(rhombus_number: @param[:msisdn])
+     merchant.id if merchant
+   end
 
    end
 
