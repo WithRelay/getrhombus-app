3ed8a4d6703db00f3c51acc35d795d30ec840fd2
Surya <surya@getrhombus.com>
2016-12-13 15:05:38 +0545

    RHOMBUSV1-156 added migration for campaign uniquness

---
+++ db/migrate/20161213071506_add_uniqueness_for_campaign_name.rb
@@ -0,0 +1,8 @@
+class AddUniquenessForCampaignName < ActiveRecord::Migration
+  def up
+    add_index :campaigns, :name, unique: true
+  end
+  def down
+    remove_index :campaigns, :name, unique: true
+  end
+end
