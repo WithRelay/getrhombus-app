46ae6255766aa2a8c61498c68bd08b46734f04a5
Surya <surya@getrhombus.com>
2016-12-14 17:11:02 +0545

    RHOMBUSV1-156 added more validation for text field

--- app/assets/javascripts/campaign_form_validator.js
+++ app/assets/javascripts/campaign_form_validator.js
@@ -6,56 +6,53 @@ $(document).on('ready page:load', function() {
     // excluded: [ ':hidden', ':not(:visible)' ],
     fields: {
       'campaign[name]': {
-          validators: {
-            notEmpty: {
-                  message: 'Campaign name is required'
-            },
-            remote: {
-                message: 'Campaign name already taken.',
-                url: '/v1/campaigns/check_campaign_name',
-                type: 'POST'
-            }
+        validators: {
+          notEmpty: {
+            message: 'Campaign name is required'
+          },
+          remote: {
+            message: 'Campaign name already taken.',
+            url: '/v1/campaigns/check_campaign_name',
+            type: 'POST'
           }
+        }
       },
-        'campaign[subject]': {
-          excluded: $('#campaign_channel').val() != 3,
-          validators: {
-            notEmpty: {
-                message: 'Subject is required'
-            }
-          }
-        },
-        'campaign[list_name]': {
-          validators: {
-            notEmpty: {
-                message: 'This Field is required'
-            }
-          }
-        }
-        ,
-        'campaign[text]': {
-          validators: {
-            notEmpty: {
-                message: 'This Field is required'
-            }
+      // 'campaign[subject]': {
+      //   enable: $('#campaign_channel').val()== 3,
+      //   validators: {
+      //     notEmpty: {
+      //       message: 'Subject is required'
+      //     }
+      //   }
+      // },
+      'campaign[list_name]': {
+        validators: {
+          notEmpty: {
+            message: 'This Field is required'
           }
         }
+      },
+      'campaign[text]': {
+        validators: {
+          notEmpty: {
+            message: 'This Field is required'
+          }
+        }
+      }
     }
   }).on('submit', function(e) {
-          $('#new_campaign').formValidation('revalidateField', 'campaign[text]');
-          $('#new_campaign').formValidation('revalidateField', 'campaign[subject]');
-
-        })
+    $('#new_campaign').formValidation('revalidateField', 'campaign[text]');
+  })
   // this is actually for campaigns not lists but uses lists
   // http://selectize.github.io/selectize.js/
 
   // For edit action, get lists data for preloading text input
   var x = $('#campaign-select-lists'),
-      campaign_lists = x.data("lists_data");
+  campaign_lists = x.data("lists_data");
 
   // Can be undefined for new action
   campaign_lists = (campaign_lists) ? campaign_lists : [];
-    var lists_selectize = x.selectize({
+  var lists_selectize = x.selectize({
     valueField: 'id',
     labelField: 'name',
     searchField: 'name',
@@ -64,19 +61,19 @@ $(document).on('ready page:load', function() {
     options: campaign_lists,
     closeAfterSelect: true,
     render: {
-        item: function(item, escape) {
-          return '<div> <span class="name">' + escape(item.name) + '</span></div>';
-        },
-        option: function(item, escape) {
-          return '<div><span class="label">' + escape(item.name) + '</span></div>';
-        }
+      item: function(item, escape) {
+        return '<div> <span class="name">' + escape(item.name) + '</span></div>';
+      },
+      option: function(item, escape) {
+        return '<div><span class="label">' + escape(item.name) + '</span></div>';
+      }
     },
     load: function(query, callback) {
       if (query.length < 2) return callback();
       $.ajax({
         url: window.location.protocol + "//" + window.location.host + "/v1/lists.json?query=" + encodeURIComponent(query),
         error: function() {
-           FlashHandler.setFlashMessage('Something went wrong...Unable to find your lists', 'error');
+          FlashHandler.setFlashMessage('Something went wrong...Unable to find your lists', 'error');
           callback();
         },
         success: function(res) { callback(res['lists']); }
@@ -84,11 +81,11 @@ $(document).on('ready page:load', function() {
     }
   })
   .on('change', function(e) {
-          $('#new_campaign').formValidation('resetField', 'campaign[list_name]');
+    $('#new_campaign').formValidation('resetField', 'campaign[list_name]');
 
-      }).on('submit', function(e) {
-          $('#new_campaign').formValidation('revalidateField', 'campaign[list_name]');
-        })
+  }).on('submit', function(e) {
+    $('#new_campaign').formValidation('revalidateField', 'campaign[list_name]');
+  })
   $( "#click-me" ).click(function() {
     alert(lists_selectize[0].selectize.getValue())
   });
