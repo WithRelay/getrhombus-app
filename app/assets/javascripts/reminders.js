205bbbe74f65a7cb3112fe5cda21ebfad6af6d65
Surya <surya@getrhombus.com>
2016-12-20 14:24:19 +0545

    RHOMBUSV1-163 created js file for reminder validation

---
+++ app/assets/javascripts/reminders.js
@@ -0,0 +1,39 @@
+$(document).on('ready page:load', function() {
+  $('#new_reminder').formValidation({
+    framework: 'bootstrap',
+    excluded: ':disabled',
+    live: 'disabled',
+    // excluded: [ ':hidden', ':not(:visible)' ],
+    fields: {
+      'reminder[channel]': {
+        validators: {
+          notEmpty: {
+            message: 'Campaign name is required'
+          }
+        }
+      },
+      'reminder[text]': {
+        validators: {
+          notEmpty: {
+            message: 'This Field is required'
+          }
+        }
+      },
+      'reminder[date_time]': {
+        validators: {
+          callback: {
+            message: 'Date and Time should be 30 minutes greate than current date time',
+            callback: function(value, validator, $field) {
+              var selectedDateTime = $('#new_reminder').find('[name="reminder[date_time]"]').val();
+              var momentDate = moment(selectedDateTime).toDate()
+              var userDateTime = new Date(new Date().getTime() + 30*60000)
+              return userDateTime < momentDate
+            }
+          }
+        }
+      }
+    }
+  }).on('change', function(e) {
+    $('#new_reminder').formValidation('resetField', 'reminder[date_time]');
+  });
+});
