ea9fb1d7b4d24f41d8d8b16ac4af19e0ca0cd074
Surya <surya@getrhombus.com>
2016-12-20 21:57:53 +0545

    RHOMBUSV1-163 implement create to create reminders

---
+++ app/controllers/api/v1/reminders_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::RemindersController < API::V1::BaseController
+  respond_to :html, :json
+
+  def create
+    @reminder = current_user.reminders.build(reminder_params)
+    saved_reminder = @reminder.save
+    json_message = saved_reminder ? { notice: 'success', status: 200 } : { error: @reminder.errors.full_messages, status: 400 }
+    render json: json_message
+  end
+
+  private
+
+  def reminder_params
+    # enums are define as integer but params are in string and rails is not converting string to integer
+    params.require(:reminder).permit(:channel, :repeat_days, :date_time, :frequency_type, :text).tap do |c|
+                                    c[:channel] = c[:channel].to_i; c[:frequency_type] = c[:frequency_type].to_i
+                                    c[:date_time] = c[:date_time].present? ? c[:date_time].in_time_zone(current_user.time_zone) : nil
+                                   end
+  end
+end
