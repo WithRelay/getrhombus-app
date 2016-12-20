49a8ad5c57124c60fba217fd05a1d4de68f62a79
Surya <surya@getrhombus.com>
2016-12-20 14:35:18 +0545

    RHOMBUSV1-163 added reminder presenter for presenter reminder logic

---
+++ app/presenters/reminder_presenter.rb
@@ -0,0 +1,7 @@
+class ReminderPresenter < BasePresenter
+  def reminder_change_status_link
+    return 'Inactive' if @model.inactive?
+    text = @model.paused? ? 'Unpause' : 'Pause'
+    h.link_to(text, change_status_user_reminder_path(@user, @model), method: :put)
+  end
+end
