0b76dd96421727265623ba993880b2d31bdfcd8c
Surya <surya@getrhombus.com>
2016-12-20 21:56:05 +0545

    RHOMBUSV1-163 delete show update create functions

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -8,29 +8,9 @@ class RemindersController < ApplicationController
     @reminder = current_user.reminders.build
   end
 
-  def create
-    @reminder = current_user.reminders.build(reminder_params)
-    if @reminder.save
-      redirect_to :back
-    else
-      render :new
-    end
-  end
-
   def edit
-    @reminder = current_user.reminders
   end
 
   def change_status
-    
-  end
-
-  private def reminder_params
-      # enums are define as integer but params are in string and rails is not converting string to integer
-      params.require(:reminder).permit(:channel, :repeat_days, :date_time, :frequency_type, :text).tap do |c|
-                                      c[:channel] = c[:channel].to_i
-                                      c[:frequency_type] = c[:frequency_type].to_i
-                                      c[:date_time] = c[:date_time].present? ? c[:date_time].in_time_zone(current_user.time_zone) : nil
-                                     end
   end
 end
