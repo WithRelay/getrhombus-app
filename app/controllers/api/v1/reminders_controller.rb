3cbc89b3b53bc7bb29c63f9ecb4f6a0cbd92bdfa
Surya <surya@getrhombus.com>
2016-12-21 14:10:52 +0545

    RHOMBUSV1-163 remove update method

--- app/controllers/api/v1/reminders_controller.rb
+++ app/controllers/api/v1/reminders_controller.rb
@@ -4,17 +4,21 @@ class Api::V1::RemindersController < API::V1::BaseController
   def create
     @reminder = current_user.reminders.build(reminder_params)
     saved_reminder = @reminder.save
-    json_message = saved_reminder ? { notice: 'success', status: 200 } : { error: @reminder.errors.full_messages, status: 400 }
-    render json: json_message
+    render json: json_message(saved_reminder).merge({ notice: 'Reminder successfully Created' })
   end
 
   private
+
+  def json_message(saved_reminder)
+    message = saved_reminder ? { status: 200 } : { error: @reminder.errors.full_messages, status: 400 }
+    return message
+  end
 
   def reminder_params
     # enums are define as integer but params are in string and rails is not converting string to integer
     params.require(:reminder).permit(:channel, :repeat_days, :date_time, :frequency_type, :text).tap do |c|
                                     c[:channel] = c[:channel].to_i; c[:frequency_type] = c[:frequency_type].to_i
                                     c[:date_time] = c[:date_time].present? ? c[:date_time].in_time_zone(current_user.time_zone) : nil
-                                   end
+                                  end
   end
 end
