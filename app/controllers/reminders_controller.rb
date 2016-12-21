727e6fd1abb672e0f1c369f842c974ba69b23995
Surya <surya@getrhombus.com>
2016-12-21 15:26:26 +0545

    RHOMBUSV1-163 set update method for reminder

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,4 +1,5 @@
 class RemindersController < ApplicationController
+  before_action :set_reminder, only: [ :edit, :update, :change_status ]
 
   def index
     @reminders = current_user.reminders
@@ -9,24 +10,35 @@ class RemindersController < ApplicationController
   end
 
   def edit
-    @reminder = current_user.reminders.find(params[:id])
   end
 
   def update
-    @reminder = current_user.reminders.find_by_id(params[:id])
     if @reminder.update(reminder_params)
       flash[:notice] = 'Reminders updated'
       # @reminder.enqueue_jobs
       redirect_to user_reminders_path(current_user)
     else
       render :edit
-      flash[:error] = @reminder.errors
+      flash[:error] = @reminder.errors.full_messages
     end
   end
 
-  def change_status; end
+  def change_status
+    status = @reminder.active? ? 2 : 1
+    if @reminder.update_attribute('status', status)
+      flash[:notice] = 'Reminder updated successfully'
+    else
+      flash[:error] = @reminder.errors.full_messages
+    end
+    redirect_to user_reminders_path(current_user)
+  end
 
   private
+
+  def set_reminder
+    @reminder = current_user.reminders.find(params[:id])
+  end
+
   def reminder_params
     # enums are define as integer but params are in string and rails is not converting string to integer
     params.require(:reminder).permit(:channel, :repeat_days, :date_time, :frequency_type, :text).tap do |c|
