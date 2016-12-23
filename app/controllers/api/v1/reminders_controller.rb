7ffac704f882ce58a389acfa086d380a01d09c40
Surya <surya@getrhombus.com>
2016-12-23 15:27:55 +0545

    RHOMBUSV1-163 set flash message for reminder

--- app/controllers/api/v1/reminders_controller.rb
+++ app/controllers/api/v1/reminders_controller.rb
@@ -5,15 +5,12 @@ class Api::V1::RemindersController < API::V1::BaseController
     @reminder = current_user.reminders.build(reminder_params)
     saved_reminder = @reminder.save
     @reminder.enqueue_notification_jobs if saved_reminder
-    render json: json_message(saved_reminder).merge({ notice: 'Reminder successfully Created' })
+    status = saved_reminder ? { status: 200 } : { status: 400 }
+    message = saved_reminder ? { notice: 'Reminder successfully Created' } : { error: @reminder.errors.full_messages }
+    render json: status.merge(message)
   end
 
   private
-
-  def json_message(saved_reminder)
-    message = saved_reminder ? { status: 200 } : { error: @reminder.errors.full_messages, status: 400 }
-    return message
-  end
 
   def reminder_params
     # enums are define as integer but params are in string and rails is not converting string to integer
