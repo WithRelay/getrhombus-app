0f06eb943ec9cd981b17da4e4b20267c95da212d
Bhishma Lamichhane <bhishma@getrhombus.com>
2016-12-23 15:58:17 +0545

    RHOMBUSV1-162 create subscription from modal

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Rails.application.routes.draw  do
     resources :fb_pages, only: [:index]
     patch 'update_fb_page' => 'fb_pages#update_user_fb_page'
     resources :hashtags, except: [:show, :destroy]
-    resources :subscriptions, only: [:index, :show, :create, :destroy] do
+    resources :subscriptions, only: [:index, :destroy] do
       collection do
         post '/:id/upgrade_subscription' => 'subscriptions#upgrade_subscription'
         post '/:id/downgrade_subscription' => 'subscriptions#downgrade_subscription'
@@ -142,6 +142,7 @@ Rails.application.routes.draw  do
     resources :coupons, only: [:index] 
     match 'plans/check_plan_name' => 'plans#check_plan_name', via: :post
     resources :plans, only: [:index, :create]
+    match 'subscriptions/create' => 'subscriptions#create', via: :post
     match 'merchant/customers' => 'merchant_customers#customers', via: :get
   end
 
