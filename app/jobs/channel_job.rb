f6833acb1fe98748f846143835cd2916b1b07b29
Surya <surya@getrhombus.com>
2016-12-27 12:05:26 +0545

    RHOMBUSV1-138 replace with ChannelCampaign::SendCampaign

--- app/jobs/channel_job.rb
+++ app/jobs/channel_job.rb
@@ -3,17 +3,7 @@ class ChannelJob
 
   def self.perform(campaign_id)
     campaign = Campaign.includes([:images, lists:[:user_lists]]).where(id: campaign_id)[0]
-    channel_class = channel_hash[campaign.channel].constantize
-    campaign.email? ? channel_class.new(campaign).send_campaign : channel_class.send_campaign(campaign)
-  end
-
-  private_class_method
-
-  def self.channel_hash
-    {
-      'sms'=>'SmsService', 'mms'=>'MmsService',
-      'facebook_messenger'=>'FacebookMessengerService',
-      'email'=>'SendEmail::EmailCampaign'
-    }
+    send_channel_campaign = ChannelCampaign::SendCampaign
+    send_channel_campaign.new(campaign).send_channel_campaign
   end
 end
