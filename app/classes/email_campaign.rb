d75efb09517b67b8317051219c9096a55d0837f8
Surya <surya@getrhombus.com>
2016-12-27 12:43:44 +0545

    RHOMBUSV1-138 comment added in code

--- app/classes/email_campaign.rb
+++ app/classes/email_campaign.rb
@@ -1,46 +1,52 @@
+# This class is only responsible for sending campaign via email channel.
 class EmailCampaign
 
-    def initialize(campaign)
-      @campaign = campaign
-    end
+  def initialize(campaign)
+    @campaign = campaign # campaign object
+  end
 
-    def user_email_list
-      email_list = []
-      @campaign.lists.each do |list|
-        list.get_users.each{ |customer| email_list.push({ email: customer[:user].email }) }
-      end
-      email_list
-    end
+  def send_campaign
+    # class method send_email_campaign accepts hash parameter
+    EmailingService.send_email_campaign(email_hash_params)
+  end
 
-    def send_campaign
-      EmailingService.send_email_campaign(email_hash_params)
-    end
+  private
 
-    private
-
-    def email_hash_params
-      message_hash = { html: @campaign.text, subject: @campaign.subject, to: user_email_list }
-      message_hash.merge!({ images: inline_images }) if inline_images.present?
-      message_hash.merge!({ attachments: attachment_images }) if attachment_images.present?
-      return message_hash
+  # returns array of user email list eg: [{ email: '<redacted_email>' }, { email: '<redacted_email>' }]
+  def user_email_list
+    email_list = []
+    @campaign.lists.each do |list|
+      list.get_users.each{ |customer| email_list.push({ email: customer[:user].email }) }
     end
+    email_list
+  end
 
-    def inline_images
-      @campaign.images.inline.map do |image|
-        @campaign.text.gsub!(image.avatar.url, "cid:#{image.avatar_file_name}")
-        create_image_params(image)
-      end
-    end
+  # returns default hash as { html: '', subject: '', to: [{email: '<redacted_email>'}]
+  # if inline image  and attachment image is present return with merging both hash
+  def email_hash_params
+    message_hash = { html: @campaign.text, subject: @campaign.subject, to: user_email_list }
+    message_hash.merge!({ images: inline_images }) if inline_images.present?
+    message_hash.merge!({ attachments: attachment_images }) if attachment_images.present?
+    return message_hash
+  end
 
-    def attachment_images
-      @campaign.images.attachment.map{ |image| create_image_params(image) }
+  # retuns [ { type: image_content_type, name: image_name,  content: Base64.encode64 } ]
+  def inline_images
+    @campaign.images.inline.map do |image|
+      @campaign.text.gsub!(image.avatar.url, "cid:#{image.avatar_file_name}")
+      create_image_params(image)
     end
+  end
 
-    def create_image_params(image)
-      {
-        type: image.avatar_content_type,
-        name: image.avatar_file_name,
-        content: Base64.encode64(open(image.avatar.url){ |image| image.read })
-      }
-    end
+  def attachment_images
+    @campaign.images.attachment.map{ |image| create_image_params(image) }
+  end
+
+  def create_image_params(image)
+    {
+      type: image.avatar_content_type,
+      name: image.avatar_file_name,
+      content: Base64.encode64(open(image.avatar.url){ |image| image.read })
+    }
+  end
 end
