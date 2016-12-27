17022526a2357a4f074cb10526d7fe22f5e19cee
Surya <surya@getrhombus.com>
2016-12-27 12:05:03 +0545

    RHOMBUSV1-138 initialize class ChannelCampaign::SendCampaign

--- app/jobs/campaign_job.rb
+++ app/jobs/campaign_job.rb
@@ -3,17 +3,7 @@ class CampaignJob < ActiveJob::Base
   queue_as :default
 
   def perform(campaign)
-    channel_class = channel_hash[campaign.channel].constantize
-    campaign.channel == 'email' ? channel_class.new(campaign).send_campaign : channel_class.send_campaign(campaign)
-  end
-
-  def channel_hash
-    # this hash is because no need to use lots of conditional statement, if you have a
-    # differenct class associate with channel please provide the appropriate class name
-    {
-      'sms'=>'SmsService', 'mms'=>'MmsService',
-      'facebook_messenger'=>'FacebookMessengerService',
-      'email'=>'SendEmail::EmailCampaign'
-    }
+    send_channel_campaign = ChannelCampaign::SendCampaign
+    send_channel_campaign.new(campaign).send_channel_campaign
   end
 end
