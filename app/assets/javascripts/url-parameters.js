aa86b6a89e51afef7c1ac5311ccb1e06f7439124
Taiwo Oyeniyi <taiwo@getrhombus.com>
2016-12-31 10:10:38 -0500

    decode entire params string...more efficient

--- app/assets/javascripts/url-parameters.js
+++ app/assets/javascripts/url-parameters.js
@@ -7,10 +7,10 @@ var PageParameter = new function() {
   function parse_page_params() {
     var params = window.location.search.substring(1), param_str; 
     if (params) {
-      params = params.split('&');
+      params = decodeURIComponent(params).split('&');
       for (var i = 0, l = params.length; i < l; i++) {
         param_str = params[i].split('=');
-        page_params[param_str[0]] = decodeURIComponent(param_str[1]);
+        page_params[param_str[0]] = param_str[1];
       }
     }
   }
