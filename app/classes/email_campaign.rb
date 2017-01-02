2459143dea1c6b7cd85bc3a033448c5de7a22d39
Surya <surya@getrhombus.com>
2017-01-02 21:57:37 +0545

    RHOMBUSV1-124 added a function send_failure

--- app/classes/email_campaign.rb
+++ app/classes/email_campaign.rb
@@ -1,33 +1,46 @@
 # This class is only responsible for sending campaign via email channel.
 class EmailCampaign
 
-  def initialize(campaign)
+  def initialize(campaign, user_list=[])
     @campaign = campaign # campaign object
+    @user_list = user_list
+    @email_list
   end
 
   def send_campaign
     # class method send_email_campaign accepts hash parameter
-    EmailingService.send_email_campaign(email_hash_params)
+    EmailingService.send_email_campaign(email_hash_params.merge({ to: user_email_list }))
+  end
+
+  def send_failure
+    EmailingService.send_email_campaign(email_hash_params.merge({ to: failure_email_list }))
   end
 
   private
+
+  def failure_email_list
+    @user_list.each do |user|
+      @email_list.push({ email: user.email })
+    end
+    @email_list
+  end
+
   # returns array of user email list eg: [{ email: '<redacted_email>' }, { email: '<redacted_email>' }]
   def user_email_list
-    email_list = []
     unless @campaign.test?
       @campaign.lists.each do |list|
-        list.get_users.each{ |customer| email_list.push({ email: customer[:user].email }) }
+        list.get_users.each{ |customer| @email_list.push({ email: customer[:user].email }) }
       end
     else
       email_list.push({ email: @campaign.user.email })
     end
-    email_list
+    return @email_list
   end
 
   # returns default hash as { html: '', subject: '', to: [{email: '<redacted_email>'}]
   # if inline image  and attachment image is present return with merging both hash
   def email_hash_params
-    message_hash = { html: @campaign.text, subject: @campaign.subject, to: user_email_list }
+    message_hash = { html: @campaign.text, subject: @campaign.subject}
     message_hash.merge!({ images: inline_images }) if inline_images.present?
     message_hash.merge!({ attachments: attachment_images }) if attachment_images.present?
     return message_hash
