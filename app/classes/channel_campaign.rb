8e9559a27324bc4b8e74af5c777eeb2b1ba2feb4
Surya <surya@getrhombus.com>
2017-01-02 14:09:20 +0545

    RHOMBUSV1-156 added a condition for send test campaign

--- app/classes/channel_campaign.rb
+++ app/classes/channel_campaign.rb
@@ -13,6 +13,12 @@ module ChannelCampaign
       # all channels classes like messenger_campaign, mobile_campaign, email_campaign
       # has a common method send campaign which send campaign to a group of users
       send_campaign = channel_class.new(@campaign).send_campaign
+      retry_other_channel(send_campaign) unless @campaign.test?
+    end
+
+    private
+
+    def retry_other_channel(send_campaign)
       unless channel_class != MessengerCampaign && send_campaign
         unless MobileCampaign.new(@campaign).send_campaign
           update_campaign if EmailCampaign.new(@campaign).send_campaign
@@ -23,8 +29,6 @@ module ChannelCampaign
         update_campaign
       end
     end
-
-    private
 
     # returns array of user_id list hash eg: [{ user_id: 1 }, { user_id: 2 }]
     def get_user_id
