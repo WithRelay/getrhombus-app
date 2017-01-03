817bde3fa96ba1e34d3cd3b387f3a6776a448add
Surya <surya@getrhombus.com>
2017-01-03 12:55:48 +0545

    RHOMBUSV1-124 return failure list if mobile campaign sms/mms failed

--- app/classes/mobile_campaign.rb
+++ app/classes/mobile_campaign.rb
@@ -1,22 +1,46 @@
 class MobileCampaign
 
-  def initialize(campaign)
+  def initialize(campaign, user_list=[])
     @campaign = campaign
+    @user_list = user_list
     @message_class = Message.new
+    @failure_list = []
+  end
+
+  def send_failure
+    @user_list.each do |user|
+      campaign_send = @message_class.send_and_save_message(rn_type, merchant_rhombus_number, user.phone_number,
+                                           message, media_link_urls)
+      @failure_list.push(customer[:user]) unless campaign_send
+    end
+    return @failure_list
   end
 
   def send_campaign
-    rn_type = @campaign.user.rn_type; media_link_urls = media_urls
-    merchant_rhombus_number = @campaign.user.rhombus_number; message = @campaign.text
+    media_link_urls = media_urls
     @campaign.lists.each do |list|
       list.get_users.each do |customer|
-        return @message_class.send_and_save_message(rn_type, merchant_rhombus_number, customer[:user].phone_number,
+        campaign_send = @message_class.send_and_save_message(rn_type, merchant_rhombus_number, customer[:user].phone_number,
                                              message, media_link_urls)
+        @failure_list.push(customer[:user]) unless  campaign_send
       end
     end
+    return @failure_list
   end
 
   private
+
+  def merchant_rhombus_number
+    @campaign.user.rhombus_number
+  end
+
+  def message
+    @campaign.text
+  end
+
+  def rn_type
+    @campaign.user.rn_type
+  end
 
   def media_urls
     @campaign.images.attachment.map{ |image| image.avatar.url }
