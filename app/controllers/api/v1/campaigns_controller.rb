c5e3460a4d8983b21b922374b250986f4689793c
Surya <surya@getrhombus.com>
2017-01-03 12:56:29 +0545

    RHOMBUSV1-124 render with flash message

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -11,8 +11,7 @@ class Api::V1::CampaignsController < API::V1::BaseController
     # we can send object to active jobs but it is good not to send complex object to active jobs
     # http://chriskottom.com/blog/2015/11/bulletproof-rails-background-jobs/
     send_email = SendNowCampaignJob.perform_now(job_params)
-    json_msg = send_email ? { notice: 'Email Send' } : { error: 'Sorry email could not send' }
-    render json: { status: 200 }.merge(json_msg)
+    render json: { status: 200, notice: 'Email Send' }
   end
 
   # Check uniqueness of campaign name from remote post request from campaign_form_validator.js
