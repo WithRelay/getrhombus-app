a026448a81b860c3d3faab9f48df763cfa96df28
Surya <surya@getrhombus.com>
2017-01-04 14:55:00 +0545

    RHOMBUSV1-124 remove sending hash send direct id

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -70,7 +70,7 @@ class Campaign < ActiveRecord::Base
     # rescue enqueue_at_with_queue accepts four parametera 1 name of queue, 2 date_time(provided as utc)
     # 3 class name 4 the parameter send for class method perform
     Resque.enqueue_at_with_queue('one_time_campaign', date_time.utc, OneTimeCampaignJob, id) if is_campaign_date_selected? || is_today_campaign?
-    SendNowCampaignJob.perform_now({ campaign_id: self.id }) if deliver_now?
+    SendNowCampaignJob.perform_now(self.id) if deliver_now?
   end
 
   private
