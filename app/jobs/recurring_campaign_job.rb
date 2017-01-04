9ec5d23868bbfe676bab62920d4005bc9130d16f
Surya <surya@getrhombus.com>
2017-01-04 22:09:26 +0545

    RHOMBUSV1-124 check date and time for scheduling in background for recurring campaign

--- app/jobs/recurring_campaign_job.rb
+++ app/jobs/recurring_campaign_job.rb
@@ -2,9 +2,12 @@ class RecurringCampaignJob
   @queue = :recurring_campaigns
 
   def self.perform
-    campaigns = Campaign.recurring.active.includes([:images, lists:[:user_lists]])
+    campaigns = Campaign.recurring.active.includes(:user)
     campaigns.each do |campaign|
-      utc_date_time = campaign.date_time.utc
+      Time.zone = campaign.user.time_zone
+      campaign_time = campaign.date_time.in_time_zone.strftime("%H:%M") + campaign.date_time.in_time_zone.strftime("%H:%M")
+      dynamic_date_time = Time.parse(campaign_time).utc
+      utc_date_time = campaign.send_count > 0 ? dynamic_date_time : campaign.date_time.utc
       Resque.enqueue_at_with_queue('one_time_campaign',
                                     utc_date_time,
                                     OneTimeCampaignJob,
@@ -16,9 +19,16 @@ class RecurringCampaignJob
 
   def self.check_date(campaign)
     Time.zone = campaign.user.time_zone
-    date_campaign = campaign.date_time.in_time_zone
-    date_now =  Time.now.in_time_zone
-    return date_now >= date_campaign
+    date_time_now =  Time.now.in_time_zone
+    job_schedule = Resque.find_delayed_selection{|s| s.include?(campaign.id) }
+    if campaign.send_count < 1
+      date_time_campaign = campaign.date_time.in_time_zone
+      job_schedule.present? ? false : date_time_now <= date_time_campaign
+    else
+      campaign_time = Time.current.strftime('%Y-%m-%d ') + campaign.date_time.in_time_zone.strftime("%H:%M")
+      dynamic_date_time = Time.parse(campaign_time).in_time_zone
+      job_schedule.present? ? false : date_time_now <= dynamic_date_time
+    end
   end
 
   def self.uncompleted_campaign_present?(campaign)
