d4cd4714136ba8de7ef1dbba2b80e5b89eb516c3
Surya <surya@getrhombus.com>
2017-01-04 12:48:11 +0545

    RHOMBUSV1-124 code refactor

--- app/jobs/send_now_campaign_job.rb
+++ app/jobs/send_now_campaign_job.rb
@@ -2,13 +2,8 @@
 class SendNowCampaignJob < ActiveJob::Base
   queue_as :send_now_campaign
 
-  def perform(job_params)
-    unless job_params[:campaign_id].present?
-      current_user = User.find_by_id(job_params[:user_id])
-      campaign = current_user.campaigns.build(job_params[:campaign_params], job_params[:image_params])
-    else
-      campaign = Campaign.find_by_id(job_params[:campaign_id])
-    end
+  def perform(campaign_id)
+    campaign = Campaign.find_by_id(campaign_id)
     ChannelCampaign::SendCampaign.new(campaign).send_channel_campaign
   end
 end
