fc133f0196abe1e447f75be3d036836ff6031f9d
Surya <surya@getrhombus.com>
2017-01-05 14:22:47 +0545

    RHOMBUSV1-124 remove unused variable

--- app/jobs/one_time_campaign_job.rb
+++ app/jobs/one_time_campaign_job.rb
@@ -3,7 +3,6 @@ class OneTimeCampaignJob
 
   def self.perform(campaign_id)
     campaign = Campaign.includes([:images, lists:[:user_lists]]).where(id: campaign_id)[0]
-    send_channel_campaign = ChannelCampaign::SendCampaign
-    send_channel_campaign.new(campaign).send_channel_campaign
+    ChannelCampaign::SendCampaign.new(campaign).send_channel_campaign
   end
 end
