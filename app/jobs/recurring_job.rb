d6685437a0b9600d280922e5c29c1149b2a40c39
Surya <surya@getrhombus.com>
2017-01-06 16:50:20 +0545

    RHOMBUSV1-124 added else if condition to check date is less than time now

--- app/jobs/recurring_job.rb
+++ app/jobs/recurring_job.rb
@@ -9,7 +9,7 @@ class RecurringJob
     if campaign.send_count < 1
       date_time_campaign = campaign.date_time.in_time_zone
       job_schedule.present? ? false : date_time_now <= date_time_campaign
-    else
+    elsif campaign.date_time <= Time.current
       campaign_time = Time.current.strftime('%Y-%m-%d ') + campaign.date_time.in_time_zone.strftime("%H:%M")
       dynamic_date_time = Time.parse(campaign_time).in_time_zone
       job_schedule.present? ? false : date_time_now <= dynamic_date_time
