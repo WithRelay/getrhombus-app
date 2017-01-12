73b8406a23f3e77ea5bbcd341ea07052a99b4e40
bhishma <itbhishma@outlook.com>
2017-01-12 08:45:21 +0545

    logic for creting business invitation through ajax call

---
+++ app/controllers/api/v1/referrers_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::ReferrersController < API::V1::BaseController
+  def invite_business
+    begin
+      status = 500
+      @referrer = Referrer.new(referrer_params)
+      if @referrer.save
+        response = 'Business invitation created successfully'
+        status = 200
+      else
+        response = 'Business invitation is not created'
+      end
+    rescue StandardError => e
+      response = 'Something went wrong on our end.'
+    end       
+    render json: { response: response }, status: status
+  end
+
+  private
+    def referrer_params
+      params.require(:referrer).permit(:referrer_name, :referrer_email, :email, :phone_number, :country, :org_name)
+    end
+end
