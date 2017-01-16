2af5edf3f48f9310f448dcbfeea8aa0a468f0042
Surya <surya@getrhombus.com>
2017-01-16 17:51:39 +0545

    RHOMBUSV1-167 Integrating relay UI with rhombus

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,7 @@ class StaticPagesController < ApplicationController
 
 	def privacy
 	end
-	
+
 	def customers
 	end
 
@@ -24,4 +24,8 @@ class StaticPagesController < ApplicationController
 
 	def to_404
 	end
+
+  def platform_integration; end
+
+  def request_demo; end
 end
