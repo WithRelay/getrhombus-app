c4b55e0b14e3da573e1221174f6a2df40e6d0add
bhishma <itbhishma@outlook.com>
2017-01-18 06:51:48 +0545

    RHOMBUSV1-133 add merchant as referrer during facebook account linking

--- app/controllers/link_fb_accounts_controller.rb
+++ app/controllers/link_fb_accounts_controller.rb
@@ -1,19 +1,22 @@
 class LinkFbAccountsController < ApplicationController
-  protect_from_forgery with: :exception
+  before_action :set_params
 
-  def link_facebook
-    @params = params
+  def link_facebook    
   end
 
   def redirect
-    @params = params
     user = User.find_by_email(@params[:email])
     if user && user.valid_password?(@params[:password])
-      FacebookMessengerService.update_user_fb_cred(@params)
+      FacebookMessengerService.update_user_fb_cred(user, @params)
       url = @params[:redirect_uri] + '&authorization_code=' + @params[:authenticity_token]
       redirect_to url
     else
       redirect_to link_facebook_path(@params), flash: { :error => "Invalid email or password" }
     end
   end
+
+  private
+    def set_params
+      @params = params
+    end
 end
