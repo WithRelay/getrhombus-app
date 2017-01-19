6f3f36e83741e3e994b5d61acdcecd308ad0cfc7
Surya <surya@getrhombus.com>
2017-01-19 11:54:45 +0545

    RHOMBUSV1-167 added a action features

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,31 +1,24 @@
+# renders all static pages as mention in its action
 class StaticPagesController < ApplicationController
+	def home; end
 
+	def about; end
 
-	def home
-	end
+	def privacy; end
 
-	def about
-	end
+	def customers; end
 
-	def privacy
-	end
+	def pricing; end
 
-	def customers
-	end
+	def faqs; end
 
-	def pricing
-	end
+	def terms; end
 
-	def faqs
-	end
-
-	def terms
-	end
-
-	def to_404
-	end
+	def to_404; end
 
   def platform_integration; end
 
   def request_demo; end
+
+  def features; end
 end
