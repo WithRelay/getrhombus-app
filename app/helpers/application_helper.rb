fce939f8ce15d15fdd6746dd821120c508ecc5f8
Surya <surya@getrhombus.com>
2017-01-19 16:03:29 +0545

    RHOMBUSV1-167 helper method check_params created to check for rendering header and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,11 @@ module ApplicationHelper
   def present(model, presenter_class=nil)
     klass = presenter_class || "#{model.class}Presenter".constantize
     presenter = klass.new(model, self, current_user)
-    if block_given?
-      yield(presenter)
-    else
-      presenter
-    end
+    block_given? ? yield(presenter) : presenter
   end
 
-
-
+  def check_params
+    restricted_actions = ['sessions-new', 'registrations-new']
+    restricted_actions.include?("#{params[:controller]}-#{params[:action]}")
+  end
 end
