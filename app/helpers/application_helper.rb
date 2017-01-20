749872ebc87e81f9c0858915ecd0f0be6b4ee706
Surya <surya@getrhombus.com>
2017-01-20 15:47:53 +0545

    create helper method to render partial header, sidebar and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,24 @@ module ApplicationHelper
     block_given? ? yield(presenter) : presenter
   end
 
+  def render_header_partial
+    concat(render 'shared/unauthenticate_header') if !check_params && unauthenticate_controller
+    concat(render 'shared/authenticated_header') unless unauthenticate_controller || check_params
+  end
+
+  def render_sidebar_partial
+    render 'shared/dashboard_sidebar' unless unauthenticate_controller || check_params
+  end
+
+  def render_footer_partial
+    render 'shared/sign_up' if !check_params && unauthenticate_controller
+    render 'shared/unauthenticate_footer' if !check_params && unauthenticate_controller
+  end
+
+  def unauthenticate_controller
+    (params[:controller]=='static_pages')
+  end
+
   def check_params
     restricted_actions = ['sessions-new', 'registrations-new']
     restricted_actions.include?("#{params[:controller]}-#{params[:action]}")
