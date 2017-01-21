25a54a49b6ea6aba36305c62bf9070134f04b718
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-01-21 14:01:30 -0500

    migration fix

--- db/migrate/20161210162213_add_index_to_list_name_and_user_id.rb
+++ db/migrate/20161210162213_add_index_to_list_name_and_user_id.rb
@@ -1,7 +1,5 @@
 class AddIndexToListNameAndUserId < ActiveRecord::Migration
   def change
   	add_index :lists, [:user_id, :name], unique: true
-  	add_index :lists, [:user_id, :name], unique: true
-
   end
 end
