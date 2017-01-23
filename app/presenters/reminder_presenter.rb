3a9403713f964536efe3b1b5702c5687c7e1d0e4
Surya <surya@getrhombus.com>
2017-01-23 16:57:10 +0545

    RHOMBUSV1-167 class added in link

--- app/presenters/reminder_presenter.rb
+++ app/presenters/reminder_presenter.rb
@@ -2,6 +2,6 @@ class ReminderPresenter < BasePresenter
   def reminder_change_status_link
     return 'Inactive' if @model.inactive?
     text = @model.paused? ? 'Unpause' : 'Pause'
-    h.link_to(text, change_status_user_reminder_path(@user, @model), method: :put)
+    h.link_to(text, change_status_user_reminder_path(@user, @model), method: :put, class: 'actions-button cancel')
   end
 end
