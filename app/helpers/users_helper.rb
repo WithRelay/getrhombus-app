873196a86ed4b0806aab38715a1c6fdcf30b69c5
Surya <surya@getrhombus.com>
2017-01-23 16:56:25 +0545

    RHOMBUSV1-167 return merchant or customer by checking their level

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def user_full_name
     current_user.first_name + ' ' + current_user.last_name
   end
+
+  def login_user_type
+    current_user.is_merchant? ? 'merchant' : 'customer'
+  end
 end
