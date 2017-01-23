847da602878052f03116d78371476c3d1ed307a5
Surya <surya@getrhombus.com>
2017-01-23 13:13:46 +0545

    RHOMBUSV1-167 fix rendering sidebar error with session create action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   def present(model, presenter_class=nil)
     klass = presenter_class || "#{model.class}Presenter".constantize
     presenter = klass.new(model, self, current_user)
