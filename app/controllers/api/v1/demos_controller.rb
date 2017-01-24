29452a21ce0be4fe421b0338e882cc1312d6b365
bhishma <itbhishma@outlook.com>
2017-01-24 07:26:28 +0545

    RHOMBUSV1-169 calandy widget

--- app/controllers/api/v1/demos_controller.rb
+++ app/controllers/api/v1/demos_controller.rb
@@ -4,18 +4,14 @@ class Api::V1::DemosController < API::V1::BaseController
   def create
     @demo = Demo.new(demo_params)
     if @demo.save
-      response = 'Thank you! Your submission has been received!'
-      status = 200
-    else
-      response = 'Oops! Something went wrong while submitting the form'
-      status = 500
+      EmailingService.send_demo_notifcation(@demo)
     end
-    render json: { response: response }, status: status
+    render json: { response: 'Thank you! Your submission has been received!' }, status: 200
   end
 
   private
 
   def demo_params
-     params.require(:demo).permit(:full_name, :company, :email, :phone, :employee_count)
+   params.require(:demo).permit(:full_name, :company, :email, :phone, :employee_count)
   end
 end
