3826b5a3c6b183f737368f624abc34db0d278711
Surya <surya@getrhombus.com>
2017-01-29 23:03:47 +0545

    RHOMBUSV1-167 route added for account_setting

--- config/routes.rb
+++ config/routes.rb
@@ -47,6 +47,7 @@ Rails.application.routes.draw  do
     get "profile", to: "devise/registrations#edit"
     get "signin", to: "devise/sessions#new"
     get "billing-information", to: "devise/registrations#billing_information"
+    get "account-setting", to: "devise/registrations#account_setting"
   end
 
   resources :contact_forms
