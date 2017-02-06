b08504ac8a6dc51674785f0086605cb3650465af
bhishma <itbhishma@outlook.com>
2017-02-06 07:09:24 +0545

    custom toastr message

--- app/assets/javascripts/flashes.js
+++ app/assets/javascripts/flashes.js
@@ -5,13 +5,35 @@ var FlashHandler = new function() {
   this.setFlashMessage = function(msg, type){
     var typeObj = { 'notice': 'success', 'warning': 'info', 'error':'error' };
     var messageToSet = typeObj[type] || 'Attention';
-    new PNotify({
-      title: messageToSet + '!!',
-      text: arrayToString(msg),
-      type: messageToSet,
-      hide: true
-    });
+    // new PNotify({
+    //   title: messageToSet + '!!',
+    //   text: arrayToString(msg),
+    //   type: messageToSet,
+    //   hide: true
+    // });
+    showToastr (messageToSet, arrayToString(msg));
+    $('.toasters-close').on('click', function (e) {
+      e.preventDefault();
+      debugger
+      $('.toasters').fadeOut(3000);
+    } );
   };
+
+  function showToastr (type, message) {
+    var class_name = (type === 'error') ? 'failure toasters' : 'toasters' ;
+    $('body').append('<div class="'+class_name+'">\
+      <div class="toaster-row w-row">\
+        <div class="toaster-row-column-1 w-col w-col-11">\
+          <div class="shrink-text toaster-text">\
+            '+message+'\
+          </div>\
+        </div>\
+        <div class="toaster-row-column-2 w-clearfix w-col w-col-1">\
+          <div class="toaster-font-awesome toasters-close"></div>\
+        </div>\
+      </div>\
+    </div>')
+  }
 
   // Confirmation Dialog for event
   var stack = {'dir1': 'down', 'dir2': 'right', 'modal': true};
