e4c6ae24335f39470a7de4cb815ad635d78afc39
Dorje <ku.dorjee@gmail.com>
2017-02-06 13:48:23 +0545

    Removed test code

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -13,6 +13,3 @@ COLORS = [
 		  	['green','#388E3C'], ['orange','#FFC107'], ['dark-grey','#607D8B'],
 		  	['red','#FF5252']
 		  ]
-
-M = 'blue'
-#A = 'silver #bdc3c7, cloud #ecf0f1, carrot #e67e22, pumpkin #d35400, peter-river #3498db, orange #f39c12, green-sea #16a085, wisteria #8e44ad, sun-flower #f1c40f, midnight-blue #2c3e50'
