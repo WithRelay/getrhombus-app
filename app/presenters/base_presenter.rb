76acfaed51c1be158a09175f073b33194b2a5001
Dorje <ku.dorjee@gmail.com>
2017-02-08 15:36:02 +0545

    UI-integration hashtag index

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -24,9 +24,11 @@ class BasePresenter < SimpleDelegator
   end
 
   def profile_image
+
       profile_pic = User.check_profile_picture(@model.user)
     if profile_pic[:type] == "image"
-       html = h.image_tag(profile_pic[:value], class: 'table-profile-picture', width: 24)
+          html = h.image_tag(profile_pic[:value], class: 'table-profile-picture', width: 24)
+          html = h.image_tag(profile_pic[:value], class: 'campaigns table-profile-picture', width: 24) if @model.class == Hashtag
       elsif profile_pic[:type] == "color"
         class_name = "campaigns table-profile-picture radius-color-#{profile_pic[:value]}"
         html = ("<div class='"+class_name+"'></div>").html_safe
