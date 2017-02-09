27cdc43e03247ea5768bbbd704b19a6786d313c1
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-02-09 08:12:57 -0500

    comments

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -35,6 +35,7 @@ class Hashtag < ActiveRecord::Base
 
 
   def mentions
+    # because hashtag_id will exist in a transaction that was created by a text message. so avoid duplicates
     in_txns_not_in_msg_count = Hashtag.find_by_sql ["SELECT count(*) as count FROM transactions t LEFT JOIN messages m
                                           on m.transaction_id = t.id
                                           WHERE t.hashtag_id = ? and m.transaction_id IS NULL", self.id].first.count
