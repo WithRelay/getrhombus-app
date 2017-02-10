44b4437c408225e9d7ef55a3e82834a1536fb820
Dorje <ku.dorjee@gmail.com>
2017-02-10 17:34:51 +0545

    Add emojioneare to textarea Saved reply

--- app/assets/javascripts/saved_replies.js
+++ app/assets/javascripts/saved_replies.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
   // bind emoji to textarea
-  var reply_body_emoji_box = $('#saved-reply-body-field').emojioneArea({
+  var reply_body_emoji_box = $('#Saved-Replies-Editor-3').emojioneArea({
     pickerPosition: 'bottom',
   });
 
