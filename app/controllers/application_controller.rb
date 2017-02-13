7491c7b134a9434dfd317c6a86c7161dfbee267f
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-02-13 08:30:25 -0500

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -67,7 +67,7 @@ class ApplicationController < ActionController::Base
 
     def set_notifications
       unless current_user.nil?
-        beginning_of_day = Time.current.beginning_of_day - 300.days
+        beginning_of_day = Time.current.beginning_of_day
         @todays_last5_txns = Transaction.get_merchant_todays_last5_txns(current_user.id, beginning_of_day)
         @todays_txns_count = Transaction.get_merchant_todays_txn_count(current_user.id, beginning_of_day)
         @todays_last_msgs_from_last5_convs = Conversation.get_last_msg_from_last5_convs(current_user.id, beginning_of_day)
