9a34d6084c0ecb82c573ab68e91b4beddc549d00
Surya <surya@getrhombus.com>
2017-02-14 13:44:36 +0545

    RHOMBUSV1-167 check user class created for authentication

---
+++ app/classes/check_user.rb
@@ -0,0 +1,21 @@
+module CheckUser
+  class RouteAuthentication
+
+    def initialize(user)
+      @user = user
+    end
+
+    def should_authenticate?
+      return user_card_token.present? if @user.is_customer?
+      return merchant_details_present?  if @user.is_merchant?
+    end
+
+    def user_card_token
+      @user.card_token
+    end
+
+    def merchant_details_present?
+      user_card_token.present? && @user.get_saas_subscription.blank? && @user.rhombus_number.blank?
+    end
+  end
+end
