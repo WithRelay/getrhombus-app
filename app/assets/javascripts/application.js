2aabec1c59db7f5a315d4a743986b2f8ee661f86
Dorje <ku.dorjee@gmail.com>
2017-02-14 10:31:32 +0545

    create Customer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -90,6 +90,7 @@
 //= require date_picker
 //= require campaign_form_validator
 //= require reminders
+//= require add-user
 //= require list_checkbox_actions
 // DO NOT REQUIRE TREE AS IT DOESNT GUARANTEE THE DEPENDENCY ORDER OF THESE FILES
 // require_tree .
