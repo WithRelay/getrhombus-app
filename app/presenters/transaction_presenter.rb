3f6a7a132e789f8a5564c2662376a3f6d1bbba07
Dorje <ku.dorjee@gmail.com>
2017-02-16 14:28:04 +0545

    transaction created date format Customer profile

--- app/presenters/transaction_presenter.rb
+++ app/presenters/transaction_presenter.rb
@@ -1,7 +1,8 @@
 class TransactionPresenter < BasePresenter
 	def format_created_at
 		time_ago = h.time_ago_in_words(@model.created_at).split(' ')
-
-    time_ago.first + time_ago.last.first + ' ago'
+		time_number = time_ago.select{|t| t.to_i != 0}
+		time_number = time_number.empty? ? [0] : time_number
+    time_number.first + time_ago.last.first + ' ago'
   end
 end
