fc1fab0dcd2ebb0e90994d864f6799dc6e0e0d39
Dorje <ku.dorjee@gmail.com>
2017-02-16 17:44:35 +0545

    Last conversation

--- app/controllers/merchant_customers_controller.rb
+++ app/controllers/merchant_customers_controller.rb
@@ -10,6 +10,10 @@ class MerchantCustomersController < ApplicationController
   def show
   	@user = User.find_by_id(params[:id])
   	@merchant_customer = MerchantCustomer.find_by_customer_id(params[:id])
+
+  	@last_conversation = Conversation.find_last_conversation(@merchant_customer.id, 'user', @user.id)
+  	@last_conversation_ref = ConversationRef.find_last_conversation_ref(@last_conversation.last) 
+
   	@transactions = Transaction.where(user_id: params[:id]).order(created_at: :desc)
   end
 end
