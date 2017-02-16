6124baeff3c4b5e83b0541f57bcbaafb99d18247
Dorje <ku.dorjee@gmail.com>
2017-02-16 14:43:39 +0545

    [Custumer Profile] bugfix format_create_at for trasaction table

--- app/presenters/transaction_presenter.rb
+++ app/presenters/transaction_presenter.rb
@@ -2,7 +2,7 @@ class TransactionPresenter < BasePresenter
 	def format_created_at
 		time_ago = h.time_ago_in_words(@model.created_at).split(' ')
 		time_number = time_ago.select{|t| t.to_i != 0}
-		time_number = time_number.empty? ? [0] : time_number
+		time_number = time_number.empty? ? ['0'] : time_number
     time_number.first + time_ago.last.first + ' ago'
   end
 end
