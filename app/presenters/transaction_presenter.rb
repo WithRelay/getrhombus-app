616e84401721216f2117e5c7014d691d1154095b
Dorje <ku.dorjee@gmail.com>
2017-02-16 14:48:14 +0545

    [Customer profile]minor changes

--- app/presenters/transaction_presenter.rb
+++ app/presenters/transaction_presenter.rb
@@ -3,6 +3,6 @@ class TransactionPresenter < BasePresenter
 		time_ago = h.time_ago_in_words(@model.created_at).split(' ')
 		time_number = time_ago.select{|t| t.to_i != 0}
 		time_number = time_number.empty? ? ['0'] : time_number
-    time_number.first + time_ago.last.first
+    time_number.first + time_ago.last.first + ' ago'
   end
 end
