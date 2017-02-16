45f09d02687319a77f87a47eab8bb5b3e5d9bb21
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-02-16 08:40:13 -0500

    merchant customer fix

--- app/controllers/merchant_customers_controller.rb
+++ app/controllers/merchant_customers_controller.rb
@@ -11,8 +11,8 @@ class MerchantCustomersController < ApplicationController
   	@user = User.find_by_id(params[:id])
   	@merchant_customer = MerchantCustomer.find_by_customer_id(params[:id])
 
-  	@last_conversation = Conversation.find_last_conversation(@merchant_customer.id, 'user', @user.id)
-  	@last_conversation_ref = ConversationRef.find_last_conversation_ref(@last_conversation.last) 
+  	@last_conversation = Conversation.find_last_conversation(@merchant_customer.merchant_id, 'user', @user.id)
+  	@last_conversation_ref = ConversationRef.find_last_conversation_ref(@last_conversation) 
 
   	@transactions = Transaction.where(user_id: params[:id]).order(created_at: :desc)
   end
