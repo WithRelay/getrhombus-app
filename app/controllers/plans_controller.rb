0d4990b235b82e8c0e971611864701ef58cc6e65
bhishma <itbhishma@outlook.com>
2017-02-18 06:34:47 +0545

    minor fix

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -9,7 +9,7 @@ class PlansController < ApplicationController
     @plans = current_user.merchant_plans
               .joins("LEFT JOIN subscriptions s ON s.plan_id = plans.id")
               .select('plans.id, amount, plans.name, currency, plans.interval, interval_count, s.id as subscription_id')
-              # .paginate(page: params[:page], per_page: 10)
+              .paginate(page: params[:page], per_page: 20)
               .order('plans.created_at DESC')
 
     respond_with(@plans)
