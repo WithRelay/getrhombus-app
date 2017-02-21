82a65c6b9ce70b0cc48bb2df2563ce19651056f2
Surya <surya@getrhombus.com>
2017-02-21 12:03:49 +0545

    RHOMBUSV1-167 remove error handling from unique record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
 
   # for uniquness check...can it be more specific...cos email, phone numbers are caught
   # capture phone number duplicates on sign up page
-  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
 
   def after_sign_in_path_for(resource)
     check_user_redirect || root_path
@@ -44,18 +43,13 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :phone_number, :password, :user_level ) }
       devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password) }
       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :current_password,
-        :password, :password_confirmation, :card_token, 
+        :password, :password_confirmation, :card_token,
         :last4, :exp_month,  :exp_year, :card_name, :card_type,
         :rhombus_number, :team_size, :use_rhombus_for, :rn_type, :rn_country,
         :phone_number, :org_name, :org_category, :org_phone, :currency,
-        :tax_percent, :url, :custom_welcome, :time_zone, :zip_code, 
-        :state_province, :city, :street_address, :country, :org_type, 
+        :tax_percent, :url, :custom_welcome, :time_zone, :zip_code,
+        :state_province, :city, :street_address, :country, :org_type,
         people_attributes: [:id, :full_name]
       )}
-    end
-
-    def record_not_unique
-      flash[:alert] = "The phone number you entered is already being used on rhombus :("
-      redirect_to "/signup"
     end
 end
