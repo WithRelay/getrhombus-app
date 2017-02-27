65ff6af0d5238198eedee8f8340858c28d4012d2
bhishma <itbhishma@outlook.com>
2017-02-27 09:17:26 +0545

    ui integration on transaction page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -11,7 +11,7 @@ class TransactionsController < ApplicationController
 
   def index
     if current_user.is_merchant?
-      @transactions = []
+      @transactions = Transaction. includes(:user).where(team_id: current_user.id)
     else      
       @transactions = []
     end
