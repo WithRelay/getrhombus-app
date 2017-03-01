abc6662f454b110a85b99d5f01937b45e424109c
Dorje <ku.dorjee@gmail.com>
2017-03-01 16:19:25 +0545

    RHOMBUSV1-189 hide customer_list_field and set customer_id on reminder modal

--- app/controllers/merchant_customers_controller.rb
+++ app/controllers/merchant_customers_controller.rb
@@ -10,10 +10,10 @@ class MerchantCustomersController < ApplicationController
 
   def show
     
-    customer_id = params[:customer_id]
-  	@user = User.find_by_id(customer_id)
-  	@merchant_customer = MerchantCustomer.find_by_customer_id(customer_id)
-    @transactions = Transaction.where(user_id: customer_id).order(created_at: :desc)
+    @customer_id = params[:customer_id]
+  	@user = User.find_by_id(@customer_id)
+  	@merchant_customer = MerchantCustomer.find_by_customer_id(@customer_id)
+    @transactions = Transaction.where(user_id: @customer_id).order(created_at: :desc)
 
     @conversations = @user.merchant_conversations
   	@last_message_resolution = @user.message_resolutions.last
--- app/helpers/customer_lists_helper.rb
+++ app/helpers/customer_lists_helper.rb
@@ -1,2 +1,9 @@
 module CustomerListsHelper
+	def customer_list_field(f)
+		if(controller.action_name == 'show' && controller.controller_name == "merchant_customers")
+	    f.hidden_field :customer_lists, value: @customer_id 
+	   else
+	    f.text_field :customer_lists, class: 'search-customers-and-contacts text-field w-input', id: 'reminder-customer-list'
+		end
+	end
 end
