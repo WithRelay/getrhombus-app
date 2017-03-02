3a0949c640888364329296c47a9314bd5d4c9423
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-03-01 22:44:30 -0500

    RHOMBUSV1-171

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -19,7 +19,7 @@ class Conversation < ActiveRecord::Base
   # Returns hash with users who sent a message to the given merchant in the last "num_days" days
   def self.get_open_conversations(merchant_id, page)
 ####convs = where(merchant_id: merchant_id, resolution: [nil, '']).paginate(page: page, per_page: 25)
-  	convs = where(merchant_id: merchant_id).paginate(page: page, per_page: 25)
+  	convs = where(merchant_id: merchant_id).paginate(page: page, per_page: 3)
   	x = convs.map { |conv| conv.conversation_hash }
     # remove these lines and x
   	x.first[:profile_image] = { type: 'image', value: 'http://lorempixel.com/400/200/' } if x.present?
@@ -182,11 +182,11 @@ class Conversation < ActiveRecord::Base
   end  
 
 
-  def self.pubish_test_conversation
+  def self.publish_test_conversation
     conversation = Conversation.first
     conv_ref = ConversationRef.first
     merchant = User.find 23
-    user = User.find 22
+    customer = User.find 22
     msg = Message.find 280
     RealtimeStreamService.publish_to_dashboard(conversation, conv_ref, merchant, customer, msg)
   end
