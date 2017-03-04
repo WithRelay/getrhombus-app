4f0e26a9a130b5138aa82efe7d128ed794257a47
bhishma <itbhishma@outlook.com>
2017-03-04 08:23:01 +0545

    fix selectize field validation on reminders

--- app/helpers/customer_lists_helper.rb
+++ app/helpers/customer_lists_helper.rb
@@ -3,7 +3,7 @@ module CustomerListsHelper
 		if (controller.action_name == 'show' && controller.controller_name == "merchant_customers")
 	    f.hidden_field :customer_id, value: @customer_id 
 	  else
-	    f.text_field :customer_id, class: 'search-customers-and-contacts text-field w-input', id: 'reminder-customer-list'
+	    f.text_field :customer_id, class: 'form-control search-customers-and-contacts text-field w-input', id: 'reminder-customer-list'
 		end
 	end
 end
