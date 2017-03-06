aa25ab7b60d218092d01074627f9b47ed70f972c
Surya <surya@getrhombus.com>
2017-03-06 16:26:59 +0545

    fix reminder unable to search customer error due to column name change in merchant contact

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < API::V1::BaseController
   def add_customers
     begin
       status = 200
-      
+
       if params[:format] == 'csv'
         # TODO
         #### review csv logic
@@ -18,7 +18,7 @@ class Api::V1::UsersController < API::V1::BaseController
           params[:user][:password] = Toolbox::StringGen.generate_random_string(8)
           params[:user][:user_level] = 0
           u = User.create(api_v1_user_params)
-          if u.errors.present? # if serverside side error occurs it returns true 
+          if u.errors.present? # if serverside side error occurs it returns true
             response = u.errors.full_messages
             status = 500
           else
@@ -50,32 +50,32 @@ class Api::V1::UsersController < API::V1::BaseController
     q = params[:query].downcase
     # uid_type is fb_page, user, phone_number
     results = User.find_by_sql [
-      "(select customer_id as uid, customer_id_type as uid_type, 
-          coalesce(NULLIF(card_name, ''), email) as title, phone_number as description from 
-        
-        (select * from 
-        
-          (select merchant_id, customer_id, 'user' as customer_id_type
+      "(select uid, uid_type,
+          coalesce(NULLIF(card_name, ''), email) as title, phone_number as description from
+
+        (select * from
+
+          (select merchant_id, customer_id, 'user' as uid_type
           from merchant_customers where merchant_id = ?
           union
-          select merchant_id, customer_id, customer_id_type
-          from merchant_contacts where merchant_id = ? and customer_id_type = 'user') as A
+          select merchant_id, uid, uid_type
+          from merchant_contacts where merchant_id = ? and uid_type = 'user') as A
 
-      inner join users on A.customer_id = users.id 
+      inner join users on A.customer_id = users.id
       where lower(card_name) like concat('%', ?, '%') or phone_number like concat('%', ?, '%')) as B)
 
       union all
 
-      (select customer_id as uid, 'fb_page', name as title, 'Messenger User'
+      (select uid, 'fb_page', name as title, 'Messenger User'
       from merchant_contacts
-      inner join fb_creds on fb_creds.page_specific_id = merchant_contacts.customer_id
-      where merchant_id = ? and customer_id_type = 'fb_page' and name <> '' and lower(name) like concat('%', ?, '%'))
+      inner join fb_creds on fb_creds.page_specific_id = merchant_contacts.uid
+      where merchant_id = ? and uid_type = 'fb_page' and name <> '' and lower(name) like concat('%', ?, '%'))
 
       union all
 
-      (select customer_id as uid, 'phone_number', 'SMS User', customer_id as description
+      (select uid, 'phone_number', 'SMS User', uid as description
       from merchant_contacts
-      where merchant_id = ? and customer_id_type = 'phone_number' and customer_id like concat('%', ?, '%'))", 
+      where merchant_id = ? and uid_type = 'phone_number' and uid like concat('%', ?, '%'))",
 
       current_user.id, current_user.id, q, q, current_user.id, q, current_user.id, q]
 
@@ -90,7 +90,7 @@ class Api::V1::UsersController < API::V1::BaseController
   private
 
     def api_v1_user_params
-      params.require(:user).permit(:email, :password, :phone_number, :user_level, :card_name, :last4, :exp_month, :exp_year, 
+      params.require(:user).permit(:email, :password, :phone_number, :user_level, :card_name, :last4, :exp_month, :exp_year,
                                     :card_token, :card_type).tap do |u|
                                                   u[:card_name] = u[:card_name].present? ? u[:card_name] : nil
                                                   u[:last4] = u[:last4].present? ? u[:last4] : nil
