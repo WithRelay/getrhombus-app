3a0633936adfbf69fd8dd2678a2c642cd2a7fd65
Surya <surya@getrhombus.com>
2017-03-06 18:04:50 +0545

    RHOMBUSV1-195 endless pagination js write with pagination default class

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,20 +1,33 @@
-// js for show more feature with will_paginate gem
-jQuery(function() {
-
-  if ($('#with-button').size() > 0) {
-
-    $('.show_more').show().click(function() {
-      var more_plans_url = $('.next_page').attr('href');
-      var $this = $(this);
-      $this.button('loading');
-      $.getScript(more_plans_url)
-        .done(function(script) {})
-        .fail(function() {
-          flashError('Sorry we couldn\'t load more plans');
-        })
-        .always(function() {
-          $this.button('reset');
-        })
+// // js for show more feature with will_paginate gem
+// jQuery(function() {
+//
+//   if ($('#with-button').size() > 0) {
+//
+//     $('.show_more').show().click(function() {
+//       var more_plans_url = $('.next_page').attr('href');
+//       var $this = $(this);
+//       $this.button('loading');
+//       $.getScript(more_plans_url)
+//         .done(function(script) {})
+//         .fail(function() {
+//           flashError('Sorry we couldn\'t load more plans');
+//         })
+//         .always(function() {
+//           $this.button('reset');
+//         })
+//     });
+//   }
+// });
+$(document).ready(function(){
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url;
+      url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Fetching more examples...");
+        return $.getScript(url);
+      }
     });
+    $(window).scroll();
   }
-});
+})
