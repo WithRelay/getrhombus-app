75d247f565ec3f15c3d9f15fee417186a6c5a942
bhishma <itbhishma@outlook.com>
2017-03-10 05:21:05 +0545

    add get_page_access_token on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -259,7 +259,11 @@ class User < ActiveRecord::Base
   def get_saas_subscription
     platform_merchant = MerchantCustomer.find_by(customer_id: self.id, merchant_id: User.get_platform_acct_obj.id)
     platform_merchant ? platform_merchant.subscriptions.active.last : nil
-    Subscription.last  #remove this
+  end
+
+  def get_page_access_token
+    page = self.fb_pages.subscribed[0]
+    page.page_access_token
   end
 
   private
