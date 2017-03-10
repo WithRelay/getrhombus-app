653844fb257c12af754fdccf814fd145a4b0dc1f
Surya <surya@getrhombus.com>
2017-03-10 18:10:23 +0545

    remove association with reminder which is already set in campaign

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,14 +1,14 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :user_lists
-  validates :name, presence:true, uniqueness: { case_sensitive: false, scope: :user_id }
+  validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :user_id }
   has_many :campaigns, through: :campaign_lists
   has_many :campaign_lists
 
   # default channel for contacting users on the list
   enum channel: [:sms, :messenger, :email]
- 
-  # List origin specifies whether the list is system generated 
+
+  # List origin specifies whether the list is system generated
   # or created by a merchant (user)
   enum origin: [:merchant, :system]
 
@@ -18,7 +18,7 @@ class List < ActiveRecord::Base
   		user_lists = User.find_by_sql([segment, {id: self.user_id}])
   		return generate_list_users user_lists, type="segment"
   	end
-  		
+
     generate_list_users self.user_lists
   end
 
@@ -29,6 +29,6 @@ class List < ActiveRecord::Base
     # @param type The type of list. Default type is "list"
     # @return An array of user objects
     def generate_list_users user_lists, type="list"
-      user_lists.map { |cus|  { user: (type == "list") ? cus.user : cus } } 
+      user_lists.map { |cus|  { user: (type == "list") ? cus.user : cus } }
     end
 end
