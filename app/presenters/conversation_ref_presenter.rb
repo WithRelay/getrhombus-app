b4f3e1dcca876c5d0cbcb8f1ddb51e3bd872613f
Dorje <ku.dorjee@gmail.com>
2017-03-14 15:21:51 +0545

    RHOMBUSV1-206 Unread preview show data in table

--- app/presenters/conversation_ref_presenter.rb
+++ app/presenters/conversation_ref_presenter.rb
@@ -8,4 +8,8 @@ class ConversationRefPresenter < BasePresenter
   	@model.resolution.present? ? 'Marked as Done' : 'Open'
   end
 
+  def display_name
+	User.get_conversation_display_name(@model.uid, @model.uid_type)   
+  end
+
 end
