833bfa53eb7f3303e291d3d65bf6baf38a0c6952
Dorje <ku.dorjee@gmail.com>
2017-03-15 15:41:04 +0545

    RHOMBUSV1-206 code refactoring in user_controller

--- app/controllers/concerns/dashboard_data.rb
+++ app/controllers/concerns/dashboard_data.rb
@@ -1,5 +1,49 @@
 module DashboardData
+	
+	def overall_section
+		customers = current_user.merchant_customers
+    new_customers = customers.select{ |c| c.created_at >= 1.week.ago.utc }
 
+    transactions = Transaction.where( team_id: current_user.id)
+    transactions_today = transactions.select{|t| t.created_at >= Time.current.beginning_of_day}
+   
+  {
+    all_customers_count: customers.count,
+    new_customers_count: new_customers.count,
+		total_transactions: transactions.sum(:amount),
+    transactions_today: transactions_today.present? ? transactions_today.sum(:amount) : 0,
+    transactions_today_count: transactions_today.count
+  }
+
+	end
+
+	def chart_and_transactions
+		# Transaction.process_captured_payment(@user, params) if current_user.user_level == 0 && params[:captured_amt].present?
+  	#data for chart, includes both fb_msg and sms
+   {
+    last6_transactions: Transaction.includes(:user).where(team_id: current_user.id).order(created_at: :desc).last(6),
+    msg_data_for_chart: all_messages_count_in_30_days
+   }
+	end
+
+	def unread_preview_section
+		{
+			message_count: Conversation.get_merchant_total_unread_msgs_count(current_user),
+	    messages_last_5: ConversationRef.get_last_msgs_from_all_merchant_convs(current_user)
+	  }
+	end
+
+	def msg_performance_section
+
+		{
+		 conversations_per_hour: Conversation.conversation_per_hour(current_user),
+    #message_count method returns hash of message_per_day , fb_percent and sms_percent  
+     messages: message_count,
+     avg_handle_time: avg_handle_time.round(2),
+		 open_convs_yesterday: open_convs_yesterday
+		}
+
+	end
 
 	#These methods below are used to collect data for merchant dashboard
   def all_messages_count_in_30_days
@@ -22,8 +66,7 @@ module DashboardData
 
     txt_msg_today = txt_msg.select {|t| t.created_at >= Time.current.beginning_of_day}
     fb_msg_today   = fb_msg.select  {|t| t.created_at >= Time.current.beginning_of_day}
-    today_msgs_count = txt_msg_today.count + fb_msg_today.count
-    @open_convs_yesterday = open_convs_yesterday 
+    today_msgs_count = txt_msg_today.count + fb_msg_today.count 
 
     fb_msg_percent = fb_msg_today.present? ? 100 * fb_msg_today.count/today_msgs_count : 0
     txt_msg_percent = txt_msg_today.present? ? 100 * txt_msg_today.count/today_msgs_count : 0
