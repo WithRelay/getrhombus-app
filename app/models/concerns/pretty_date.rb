a6898d62cf48d93c4002229183efe59a32014063
surya@getrhombus.com <surya@getrhombus.com>
2017-03-16 22:51:20 +0545

    remove commented binding.pry

--- app/models/concerns/pretty_date.rb
+++ app/models/concerns/pretty_date.rb
@@ -5,9 +5,8 @@ module PrettyDate
     short_format = { less_then_sec: '1s', sec: 's', min: 'm', hour: 'h', day: 'd', week: 'w', }
     medium_format = { less_then_sec: '1 sec', sec: 'sec', min: 'mins', hour: 'hrs', day: 'days', week: 'week'}
     long_format = { less_then_sec: '1 sec ago', sec: 'sec ago', min: 'minuntes ago', hour: 'hours ago', day: 'days ago', week: 'weeks ago'}
-    
+
     format = eval(format_params)
-    # binding.pry
     case a
       when 0..1 then format[:less_then_sec]
       when 2..59 then a.to_s +  format[:sec]
@@ -15,6 +14,6 @@ module PrettyDate
       when 3600..86399 then (a/(60*60)).to_i.to_s + format[:hour]
       when 86400..604799 then (a/(60*60*24)).to_i.to_s + format[:day]
       else (a/(60*60*24*7)).to_i.to_s + format[:week]
-    end  
+    end
   end
 end
