9fa3d342719a7118561fec728a3a3c9a6cf7a550
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-03-16 22:14:06 -0400

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316060144) do
+ActiveRecord::Schema.define(version: 20170317021017) do
+
+  create_table "account_reloads", force: :cascade do |t|
+    t.integer  "user_id",        limit: 4
+    t.integer  "transaction_id", limit: 4
+    t.integer  "origin",         limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street_address",   limit: 191
