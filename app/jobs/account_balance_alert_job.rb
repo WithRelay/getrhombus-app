85959dd1b8c29e35797a2a75d8340aed72f76aaa
bhishma <itbhishma@outlook.com>
2017-03-17 07:43:53 +0545

    RHOMBUSV1-209 move auto reload action to the job

--- app/jobs/account_balance_alert_job.rb
+++ app/jobs/account_balance_alert_job.rb
@@ -4,9 +4,16 @@ class AccountBalanceAlertJob
 	def self.perform
 		User.where(user_level: 1).each do |user|
 			if user.account_balance < 5
+				recharge(user.auto_reload_amt) if user.auto_reload
 				EmailingService.account_balance_alert(user)
 			end
 		end
+	end
+
+	private
+
+	def recharge(amt)
+		# recharge logic
 	end
 
 end
