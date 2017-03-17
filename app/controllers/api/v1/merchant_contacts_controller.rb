0428a9fd75cc755fc6ccce7a968ffe4f476df84e
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-03-17 02:00:58 -0400

    clean up

---
+++ app/controllers/api/v1/merchant_contacts_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::MerchantCustomersController < API::V1::BaseController
+
+
+  # all contacts (optional parameter to filter based on channel)
+  def index
+  end
+
+
+
+end
--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::MerchantCustomersController < API::V1::BaseController
+  
+  # this is really customers with payment info
   def customers
     begin
       customers =  MerchantCustomer.joins(:customer)
@@ -13,20 +15,8 @@ class Api::V1::MerchantCustomersController < API::V1::BaseController
     end
   end
 
-  ## probably can remove this
-  def find
-    sql = ActiveRecord::Base.send(:sanitize_sql_array,
-    ["SELECT users.card_name, users.phone_number FROM
-          ( SELECT user_id as usersID FROM messages where messages.user_id_to = ?
-              union
-            SELECT user_id_to as usersID FROM messages where messages.user_id = ?
-          ) t1
-          inner join users on t1.usersID = users.id where lower(card_name) LIKE concat('%', ?, '%') or
-          phone_number like concat('%', ?, '%') and card_token is not null", current_user.id, current_user.id, params[:query].downcase, params[:query] ])
-
-    results = User.connection.select_all(sql)
-    results = results.map { |u| { phone_number: u["phone_number"], card_name: u['card_name'] } }
-    render json: { "users" => results }, status: 200
+  # all customers - with or without payment info
+  def index
   end
 
 end
