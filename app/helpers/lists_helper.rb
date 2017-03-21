d57debd686af2909996fe8a03d83d26874624456
surya@getrhombus.com <surya@getrhombus.com>
2017-03-21 15:51:33 +0545

    for setting channel in hidden field added a helper method get_list_channel

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,3 +1,10 @@
 module ListsHelper
-
+  def get_list_channel
+    value = if params[:uid_type].present?
+              params[:uid_type]
+            elsif params[:action] == "leads_contacts" && params[:uid_type].nil?
+              'sms'
+            end
+    return value
+  end
 end
