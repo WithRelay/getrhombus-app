bd2ca23b2a23618460d8cbb4c8110edd9632f296
Dorje <ku.dorjee@gmail.com>
2017-03-22 12:52:33 +0545

    bugfix for double rendring Campaing/index page

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -11,11 +11,14 @@ class CampaignsController < ApplicationController
     @campaigns = current_user.campaigns.paginate(per_page: PAGINATION_PER_PAGE,
                                                  page: params[:page])
                                                  .order('updated_at DESC')
-   respond_to do |format|
-     format.js { render partial: 'shared/index.js.erb', locals: { obj: @campaigns } }
-      format.html
-    end
     render 'empty_campaign' unless @campaigns.present?
+
+      if @campaigns.present?
+        respond_to do |format|
+          format.js { render partial: 'shared/index.js.erb', locals: { obj: @campaigns } }
+          format.html
+        end
+      end
   end
 
   # initializing campaign as association way using build method.
