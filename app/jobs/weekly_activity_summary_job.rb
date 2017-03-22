33176096838f155f73e9ab7b0a4b2902a09f9fa4
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-03-22 09:37:33 -0400

    fixes to weekly summary email

--- app/jobs/weekly_activity_summary_job.rb
+++ app/jobs/weekly_activity_summary_job.rb
@@ -1,13 +1,24 @@
 class WeeklyActivitySummaryJob
 
+  include DashboardData
+  include UserProfile
+
   @queue = :weekly_activity_summary
 
   def self.perform
-    
-    User.where(user_level: 0).each do |user|
-      
-      time_in_zone = Time.current
-      EmailingService.weekly_activity_summary(user)
+    merchants = User.where(user_level: 1)
+    merchants.each do |merchant|
+      mail_data = mail_data_to_merchant(merchant)
+      # sendmail(last_week_customers)
+    end
+  end
+
+#  private
+  def mail_data_to_merchant(merchant)
+    Time.zone = merchant.time_zone
+    last_week_customers = merchant.customers.where('merchant_customers.created_at >= ?', 7.days.ago.utc)
+    #have to use UserProfile for fullname/ profile etc.
+    { merchant_name: merchant.first_name, merchant_email: merchant.email, customers_list: last_week_customers }
   end
 
 end
