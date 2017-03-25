b1f3166787a209a5cbe0318560a34673da47dd2c
surya@getrhombus.com <surya@getrhombus.com>
2017-03-25 16:20:04 +0545

    fix ui stuffs with pai

--- app/controllers/api/v1/demos_controller.rb
+++ app/controllers/api/v1/demos_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::DemosController < API::V1::BaseController
 
   def create
-    @demo = Demo.new(demo_params)
-    EmailingService.send_demo_notifcation(@demo) if @demo.save
+    Demo.create(demo_params)
     render json: { response: 'Thank you! Your submission has been received!' }, status: 200
   end
 
