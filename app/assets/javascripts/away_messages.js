4532e039bf5b4a5c9663cdf36ceb29cba5e7f314
surya@getrhombus.com <surya@getrhombus.com>
2017-03-27 13:16:20 +0545

    validation added for away message

--- app/assets/javascripts/away_messages.js
+++ app/assets/javascripts/away_messages.js
@@ -15,6 +15,16 @@ $(document).ready(function(){
       $('#away-message-text-area').slideDown();
   });
 
+  $('#save-away-message-button').click(function(e){
+    if ($('#enable-away-message').is(':checked') && $(autoResponseTextBox).val() == ''){
+      $('.emojionearea').attr('style', 'border: 1px solid red;')
+      e.preventDefault();
+    }
+    else{
+      $('.emojionearea').removeAttr('style')
+    }
+  });
+
   function enableEmojiCounter(element){
     $(element).emojioneArea();
   }
