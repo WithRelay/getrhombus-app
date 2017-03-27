7462409b91cd14a9ce5ed400c3f6897b1ca6d8dd
surya@getrhombus.com <surya@getrhombus.com>
2017-03-27 16:54:27 +0545

    added a method update

--- app/controllers/away_messages_controller.rb
+++ app/controllers/away_messages_controller.rb
@@ -3,22 +3,32 @@ class AwayMessagesController < ApplicationController
   include DashboardNotification
   before_action :set_notifications
 
-  def new
-    @away_message = current_user.away_messages.build
+  def show
+    away_message = current_user.away_message
+    @away_message = away_message.present? ? away_message : current_user.build_away_message
   end
 
   def create
-    @away_message = current_user.away_messages.build(away_message_params)
-    if @away_message.save
-      flash[:notice] = 'Away message saved successfully'
-      redirect_to new_user_away_message_path(current_user)
-    else
-      flash[:error] = 'Away message could not save'
-      render :new
-    end
+    @away_message = current_user.build_away_message(away_message_params)
+    @away_message.save ? success_message_with_path : error_mesage_with_path
+  end
+
+  def update
+    @away_message = current_user.away_message.update_attributes(away_message_params)
+    @away_message ? success_message_with_path : error_mesage_with_path
   end
 
   private
+
+  def success_message_with_path
+    flash[:notice] = 'Away message saved successfully'
+    redirect_to user_away_message_path(current_user)
+  end
+
+  def error_mesage_with_path
+    flash[:error] = 'Away message could not save'
+    render :show
+  end
 
   def away_message_params
     params.require(:away_message).permit(:enabled, :response, :mon_ct, :mon_ot, :tue_ct, :wed_ct,
