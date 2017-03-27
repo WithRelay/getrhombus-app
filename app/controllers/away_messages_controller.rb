da7e068f90d243d4fd69173ba0dd2d0e928eb71a
surya@getrhombus.com <surya@getrhombus.com>
2017-03-27 12:57:32 +0545

    create and new method added

--- app/controllers/away_messages_controller.rb
+++ app/controllers/away_messages_controller.rb
@@ -3,7 +3,26 @@ class AwayMessagesController < ApplicationController
   include DashboardNotification
   before_action :set_notifications
 
-  def index
+  def new
     @away_message = current_user.away_messages.build
+  end
+
+  def create
+    @away_message = current_user.away_messages.build(away_message_params)
+    if @away_message.save
+      flash[:notice] = 'Away message saved successfully'
+      redirect_to new_user_away_message_path(current_user)
+    else
+      flash[:error] = 'Away message could not save'
+      render :new
+    end
+  end
+
+  private
+
+  def away_message_params
+    params.require(:away_message).permit(:enabled, :response, :mon_ct, :mon_ot, :tue_ct, :wed_ct,
+                                         :wed_ot, :thurs_ct, :thurs_ot, :fri_ct, :fri_ot, :sat_ct,
+                                         :sun_ct, :sun_ot)
   end
 end
