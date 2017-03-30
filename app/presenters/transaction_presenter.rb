4480afb5153dd8e438d4cef5206192f75301e96b
Bhishma Lamichhane <bhishmalamichhane@Bhishmas-MacBook-Pro.local>
2017-03-30 07:42:40 +0545

    customer name and profile dashboard link bug fix on transaction index

--- app/presenters/transaction_presenter.rb
+++ app/presenters/transaction_presenter.rb
@@ -4,4 +4,10 @@ class TransactionPresenter < BasePresenter
     return '-' if @model.amount_with_taxes.blank?
     "$" + Toolbox::Decimal.to_2dp(@model.amount_with_taxes)
   end
+
+
+  def transaction_user_name
+    return @model.user.full_name if @model.user.full_name.present?
+    @model.user.email
+  end
 end
