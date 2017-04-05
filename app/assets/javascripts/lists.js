bce6db8cb7a23e57ba42ff0dc6f7b0ca04da08a0
surya@getrhombus.com <surya@getrhombus.com>
2017-04-05 14:07:50 +0545

    trigger to the segment show according to the segment selected in sidebar

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -56,9 +56,14 @@ $(document).on('ready page:load', function() {
         ))
       });
   }).fail(function(msg){
-    debugger;
-
     setFlashForList(msg, 'error');
+  });
+
+  $('#Segment-Select-lists').on('change', function(){
+    if (this.value){
+      var window_location = window.location.pathname.split('/')
+      window.location = '/' + window_location[1] + '/' + window_location[2]  + '/lists/' + this.value + '/segment_list';
+    }
   });
 
   $("#edit-selected-list").click(function(e){
