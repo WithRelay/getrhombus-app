88bb1fb34bdbebe924270f05104df31dec9de04e
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-04-06 18:06:29 -0400

    fix customer index url

--- app/models/user.rb
+++ app/models/user.rb
@@ -118,7 +118,7 @@ class User < ActiveRecord::Base
   before_validation :the_titleizer
   before_create :set_merchant_org_phone          # only create because the actual org_phone field is used in edit view
 
-  after_commit :do_signup_stuff, on: :create
+  #after_commit :do_signup_stuff, on: :create
 
   enum status: { inactive: 0, active: 1 }
 
