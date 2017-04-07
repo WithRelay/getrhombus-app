2c13819ce0e71585521313527a0300db577784c4
Bhishma Lamichhane <bhishma@getrhombus.com>
2017-04-07 07:06:07 +0545

    refactor missing method

--- app/services/emailing_service.rb
+++ app/services/emailing_service.rb
@@ -347,6 +347,7 @@ class EmailingService
         template_name = 'unread-messages'
         template_content = []
         message = { "subject"=>"Unread Message Notification",
+         "global_merge_vars"=> [ { "name" => "customer_first_name", "content" => options[:customer_first_name] },
          "global_merge_vars"=> [ { "name" => "unread_count", "content" => options[:unread_count] },
                                  { "name" => "pluralize_msg", "content" => options[:pluralize_msg] } ],
          "merge_language" => "handlebars",
