9dcf961f9b253537c985197290a9895c169879f4
surya@getrhombus.com <surya@getrhombus.com>
2017-04-11 13:59:21 +0545

    change path from merchant_contacts to contacts

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -65,14 +65,14 @@ $(document).on('ready page:load', function() {
   $('#contacts-segment-list').on('change', function(){
     if (this.value){
       var window_location = window.location.pathname.split('/')
-      window.location = '/' + window_location[1] + '/' + window_location[2]  + '/merchant_contacts/' + this.value + '/segment_users';
+      window.location = '/' + window_location[1] + '/' + window_location[2]  + '/contacts/' + this.value + '/segment_users';
     }
   });
 
   $('#Segment-Select-lists').on('change', function(){
     if (this.value){
       var window_location = window.location.pathname.split('/')
-      window.location = '/' + window_location[1] + '/' + window_location[2]  + '/merchant_customers/' + this.value + '/segment_users';
+      window.location = '/' + window_location[1] + '/' + window_location[2]  + '/customers/' + this.value + '/segment_users';
     }
   });
 
