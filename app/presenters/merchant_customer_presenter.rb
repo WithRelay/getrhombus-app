fdac199eec7e37c0df393b129999b9be244a0063
Dorje <torchi@getrhombus.com>
2017-04-13 16:50:49 +0545

    profile_image dyanamic for merchant and customer

--- app/presenters/merchant_customer_presenter.rb
+++ app/presenters/merchant_customer_presenter.rb
@@ -1,7 +1,8 @@
 class MerchantCustomerPresenter < BasePresenter
-  
-  def profile_image
-    profile_pic = User.check_profile_picture(@model.customer)
+
+  def profile_image(current_user)
+    user = current_user.is_merchant? ? @model.customer : @model.merchant
+    profile_pic = User.check_profile_picture(user)
     if profile_pic[:type] == "image"
       html = h.image_tag(profile_pic[:value], class: 'campaigns table-profile-picture', width: 24)
     elsif profile_pic[:type] == "color"
