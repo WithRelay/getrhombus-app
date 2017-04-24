ed4d372cff2738262175a7f68270adddee7814eb
Dorje <torchi@getrhombus.com>
2017-04-24 18:09:59 +0545

    add create and formvalidation code

--- app/assets/javascripts/saved_replies.js
+++ app/assets/javascripts/saved_replies.js
@@ -9,4 +9,58 @@ $(document).ready(function() {
   var reply_body_emoji_box = $('#Edit-Saved-Replies-Editor').emojioneArea({
     pickerPosition: 'bottom',
   });
+
+  function create_saved_reply(formData){
+    $.ajax({
+      url: window.location.origin + "/v1/saved_replies",
+      method: "POST",
+      data: formData,
+      dataType: 'json'
+    }).done(function(res){
+      FlashHandler.setFlashMessage(res.notice,'notice');
+      location.reload();
+      }).error(function(res){
+        FlashHandler.setFlashMessage(res.error, 'error');
+    });
+  }
+
+  //if ($('.save-reply-form').length) {
+    $('#save-reply-form').formValidation({
+      framework: 'bootstrap',
+      excluded: ':disabled',
+      live: 'disabled',
+      err: {
+            container: function($field, validator) {
+                return $field.parent().find('.messageContainer');
+            }
+        },
+      fields: {
+        'saved_reply[body]': {
+          validators: {
+            notEmpty: {
+              message: 'This Field is required'
+            }
+          }
+        },
+
+        'saved_reply[title]': {
+          validators: {
+            notEmpty: {
+              message: 'This Field is required'
+            }
+          }
+        },
+
+        }
+    }).on('success.form.fv', function(e, data) {
+      if (this.id != 'edit-save-reply-form'){
+        e.preventDefault();
+        create_saved_reply($(this).serialize());
+      }
+
+      $('.update-close-modals').click();
+    });
+  // }
+
+
 });
