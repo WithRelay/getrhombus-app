e9de01cf55daae75b910d84bfb714373bb4c26b7
surya@getrhombus.com <surya@getrhombus.com>
2017-04-25 17:39:45 +0545

    campaign message for notification added

--- app/services/realtime_stream_service.rb
+++ app/services/realtime_stream_service.rb
@@ -9,19 +9,19 @@ class RealtimeStreamService
       $pubnub.subscribe(channel: 'messaging_' + Rails.env + '_' + merchant_id) {}
       $pubnub.publish(channel: 'messaging_' + Rails.env + '_' + merchant_id,
                       message: { type: 'new-message',
-                                 message: Conversation.message_hash(conversation, msg, conv_ref, customer), 
+                                 message: Conversation.message_hash(conversation, msg, conv_ref, customer),
                                  conversation: conversation.conversation_hash }.to_json) {}
     end
 
     def update_conversation_properties(conversation_id, customer, merchant_id, old_selectize_val)
       $pubnub.publish(channel: 'messaging_' + Rails.env + '_' + merchant_id.to_s,
-                      message: { type: 'update-conv-properties', id: conversation_id, 
-                                 full_name: User.get_conversation_display_name(customer.id, "user"), 
+                      message: { type: 'update-conv-properties', id: conversation_id,
+                                 full_name: User.get_conversation_display_name(customer.id, "user"),
                                  profile_image: User.check_profile_picture(customer),
                                  old_selectize_val: old_selectize_val,
                                  selectize: { uid: customer.id, uid_type: 'user',
                                               description: customer.phone_number,
-                                              unique_identifier: "#{customer.id}-user", 
+                                              unique_identifier: "#{customer.id}-user",
                                               title: customer.card_name.present? ? customer.card_name : customer.email }
                                 }.to_json) {}
     end
@@ -35,6 +35,15 @@ class RealtimeStreamService
                       message: payload.to_json) {}
     end
 
+    def campaign_notification
+      Time.zone = current_user.time_zone
+      campaign_user_list = CampaignUserList.where("CAST(created_at as DATE) = ?",
+                           Time.current.in_time_zone.strftime("%Y-%m-%d"))
+      campaign_user_list.each do |list|
+        list.campaign.campaign_lists.count
+        campaign_time = list.campaign.date_time.strftime("%I:%M")
+        { campaign_sent: "Your campaign scheduled for #{campaign_time} was sent"}
+      end
+    end
   end
-  
 end
