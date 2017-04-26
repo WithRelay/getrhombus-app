7b887b2907f1eabeb9d7de93e894a7eb7edaf4ff
Dorje <torchi@getrhombus.com>
2017-04-26 16:22:45 +0545

    merge overriden method profile_image to base method

--- app/presenters/merchant_customer_presenter.rb
+++ app/presenters/merchant_customer_presenter.rb
@@ -1,15 +1,15 @@
 class MerchantCustomerPresenter < BasePresenter
 
-  def profile_image(current_user)
-    user = current_user.is_merchant? ? @model.customer : @model.merchant
-    profile_pic = User.check_profile_picture(user)
-    if profile_pic[:type] == "image"
-      html = h.image_tag(profile_pic[:value], class: 'campaigns table-profile-picture', width: 24)
-    elsif profile_pic[:type] == "color"
-      class_name = " campaigns table-profile-picture radius-color-#{profile_pic[:value]}"
-      html = ("<div class='"+ class_name +"'></div>").html_safe
-    end
-    html
-  end
+  # def profile_image(current_user)
+  #   user = current_user.is_merchant? ? @model.customer : @model.merchant
+  #   profile_pic = User.check_profile_picture(user)
+  #   if profile_pic[:type] == "image"
+  #     html = h.image_tag(profile_pic[:value], class: 'campaigns table-profile-picture', width: 24)
+  #   elsif profile_pic[:type] == "color"
+  #     class_name = " campaigns table-profile-picture radius-color-#{profile_pic[:value]}"
+  #     html = ("<div class='"+ class_name +"'></div>").html_safe
+  #   end
+  #   html
+  # end
 
 end
