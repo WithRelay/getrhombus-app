5bd7e053ec2d18c24d034c608be4a9758bb0d4fc
surya@getrhombus.com <surya@getrhombus.com>
2017-04-27 17:06:29 +0545

    contact sending and customer sending campaign separated

--- app/classes/mobile_campaign.rb
+++ app/classes/mobile_campaign.rb
@@ -3,14 +3,14 @@ class MobileCampaign
   def initialize(campaign, user_list=[])
     @campaign = campaign
     @user_list = user_list
-    @message_class = Message.new
+    @message_obj = Message.new
     @failure_list = []
   end
 
   def send_failure
     media_link_urls = media_urls
     @user_list.each do |user|
-      campaign_send = @message_class.send_and_save_message(rn_type, merchant_rhombus_number, user.phone_number,
+      campaign_send = @message_obj.send_and_save_message(rn_type, merchant_rhombus_number, user.phone_number,
                                            message, media_link_urls)
       @failure_list.push(user) unless campaign_send && list.channel.present?
     end
@@ -18,18 +18,33 @@ class MobileCampaign
   end
 
   def send_campaign
-    media_link_urls = media_urls
     @campaign.lists.each do |list|
+      contact_list = list.contact?
       list.get_users.each do |customer|
-        campaign_send = @message_class.send_and_save_message(rn_type, merchant_rhombus_number, customer[:user].phone_number,
-                                             message, media_link_urls)
-        @failure_list.push(customer[:user]) unless campaign_send && list.channel.present?
+        campaign_send = contact_list ? send_to_contact(customer[:user]) : send_to_customer(customer[:user])
+        @failure_list.push(customer[:user]) if (!contact_list && campaign_send && list.channel.present?)
       end
     end
     return @failure_list
   end
 
   private
+
+  def send_to_contact(merchant_contact_obj)
+    contact_details = User.get_user_snapshot(merchant_contact_obj.uid, merchant_contact_obj.uid_type,
+                                            @campaign.user.id)
+    send_by_mobile(contact_details[:phone_number])
+  end
+
+  def send_to_customer(user_obj)
+    send_by_mobile(user_obj.phone_number)
+  end
+
+  def send_by_mobile(phone_number)
+    media_link_urls = media_urls
+    @message_obj.send_and_save_message(rn_type, merchant_rhombus_number, phone_number,
+                                         message, media_link_urls)
+  end
 
   def merchant_rhombus_number
     @campaign.user.rhombus_number
