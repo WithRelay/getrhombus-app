ec512eeca78d42c296e1e43284ab19f193e78461
surya@getrhombus.com <surya@getrhombus.com>
2017-04-27 17:07:19 +0545

    refactor helper method that return list channel

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,11 +1,6 @@
 module ListsHelper
   def get_list_channel
-    value = if params[:uid_type].present?
-              params[:uid_type]
-            elsif params[:action] == "leads_contacts" && params[:uid_type].nil?
-              'sms'
-            end
-    return value
+    params[:uid_type].present? ? params[:uid_type] : 'sms'
   end
 
   def customer_contact_details(list_user)
