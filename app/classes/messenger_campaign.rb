7f915793a130e8c9d212be42c0da12805d2d415b
surya@getrhombus.com <surya@getrhombus.com>
2017-05-04 16:55:29 +0545

    User label argument in sending campaign

--- app/classes/messenger_campaign.rb
+++ app/classes/messenger_campaign.rb
@@ -12,15 +12,7 @@ class MessengerCampaign
     failure_list = []
     @campaign.lists.each do |list|
       list.get_users.each do |customer|
-        send_message = if customer[:user].is_a?(User)
-                          Conversation.find_or_create_conversation_for_message_and_send_publish(
-                                    @campaign.user, customer, '', '',
-                                    @campaign.text, '', media = [])
-                      else
-                         Conversation.find_or_create_conversation_for_message_and_send_publish(
-                                   @campaign.user, nil, customer[:user].uid,
-                                   @campaign.text, customer[:user].uid_type)
-                      end
+
       failure_list.push(customer[:user]) unless send_message && list.channel.present?
       end
     end
@@ -28,6 +20,20 @@ class MessengerCampaign
   end
 
  private
+
+ def send_to_contact(merchant_contact_obj)
+   send_fb_message(uid_type: merchant_contact_obj.uid_type, uid: merchant_contact_obj.uid)
+ end
+
+ def send_to_customer(user_obj)
+   send_fb_message(customer: user_obj)
+ end
+
+ def send_fb_message(customer: nil, uid_type: nil, uid: nil)
+   media_link_urls = media_urls
+   Conversation.find_or_create_conversation_for_message_and_send_publish(
+                @campaign.user, customer, uid_type, uid, @campaign.text, 'SMS', media_link_urls)
+ end
 
   def get_page_specific_id
     res = nil
@@ -38,21 +44,25 @@ class MessengerCampaign
     res
   end
 
-  def send_fb_images(page_token, fb_cred_id)
-    @campaign.images.attachment.each do |image|
-      @facebook_messenger.send_attachment(page_token, fb_cred_id, 'image', image.avatar.url)
-    end
+  def media_urls
+    @campaign.images.attachment.map{ |image| image.avatar.url }
   end
 
-  # get merchant fb pages which are subscribed
-  def user_subscribed_pages
-    @campaign.user.fb_pages.subscribed
-  end
+  # def send_fb_images(page_token, fb_cred_id)
+  #   @campaign.images.attachment.each do |image|
+  #     @facebook_messenger.send_attachment(page_token, fb_cred_id, 'image', image.avatar.url)
+  #   end
+  # end
+  #
+  # # get merchant fb pages which are subscribed
+  # def user_subscribed_pages
+  #   @campaign.user.fb_pages.subscribed
+  # end
 
-  def user_page_access_token
-    subscribed_pages = user_subscribed_pages
-    subscribed_pages[0].page_access_token if subscribed_pages.present?
-  end
+  # def user_page_access_token
+  #   subscribed_pages = user_subscribed_pages
+  #   subscribed_pages[0].page_access_token if subscribed_pages.present?
+  # end
 
   # def fb_message_sender(token, fb_id)
   #   token_fb_id = (token.present? && fb_id.present?)
