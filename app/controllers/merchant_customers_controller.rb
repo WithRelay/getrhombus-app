f96594828e800fecbcf869588df980147a5f43c9
Dorje <torchi@getrhombus.com>
2017-05-04 17:50:00 +0545

    add pagination to 15 customers

--- app/controllers/merchant_customers_controller.rb
+++ app/controllers/merchant_customers_controller.rb
@@ -6,7 +6,7 @@ class MerchantCustomersController < ApplicationController
 
   def index
     @customers = current_user.merchant_customers
-                              #.paginate(page: params[:page], per_page: 10).order(created_at: :desc)
+                             .paginate(page: params[:page], per_page: 15).order(:id)
     @new_customer = User.new
     @customers.present? ? render_requested_format(@customers) : render(:empty_customer)
   end
