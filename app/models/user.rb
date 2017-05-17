62ffff880d56dd3b0121de17959d9f8dd2f8c86d
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-05-17 15:45:23 -0400

    fix how we check if merchant has saas plan

--- app/models/user.rb
+++ app/models/user.rb
@@ -202,7 +202,7 @@ class User < ActiveRecord::Base
 
   def get_saas_subscription
     platform_merchant = MerchantCustomer.find_by(customer_id: self.id, merchant_id: User.get_platform_acct_obj.id)
-    platform_merchant ? platform_merchant.subscriptions.active.last : nil
+    platform_merchant ? platform_merchant.subscriptions.last : nil
   end
 
   def get_page_access_token
