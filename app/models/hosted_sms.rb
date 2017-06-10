243109405e7ebf43add72983d38dca13aa899d9c
Bhishma Lamichhane <bhishma@getrhombus.com>
2017-06-10 05:46:21 +0545

    socpe for signature unsigned for hosted sms

--- app/models/hosted_sms.rb
+++ app/models/hosted_sms.rb
@@ -1,4 +1,7 @@
 class HostedSms < ActiveRecord::Base
   serialize :cc_emails, Array
   serialize :capabilities
+
+  #  Letter of Authorization (LOA) document the user needs to sign.
+  scope :unsigned, -> { where(signing_document_sid: nil) }
 end
