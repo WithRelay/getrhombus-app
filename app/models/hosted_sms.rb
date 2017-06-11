e5c11a843b14854994e50347ac0d72be4bde763a
Bhishma Lamichhane <bhishma@getrhombus.com>
2017-06-12 05:00:46 +0545

    serialize status event

--- app/models/hosted_sms.rb
+++ app/models/hosted_sms.rb
@@ -1,6 +1,7 @@
 class HostedSms < ActiveRecord::Base
   serialize :cc_emails, Array
   serialize :capabilities
+  serialize :status_events
 
   #  Letter of Authorization (LOA) document the user needs to sign.
   scope :unsigned, -> { where(signing_document_sid: nil) }
