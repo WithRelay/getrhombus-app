74ac9bd768db7dc4757650e44460e319f6f6f148
Bhishma Lamichhane <bhishma@getrhombus.com>
2017-06-21 06:18:48 +0545

    Fix css on show more button loading

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -8,7 +8,8 @@ jQuery(function() {
       var $this = $(this);
       var content = $this.html();
       var data_loading_text = $this.attr('data-loading-text');
-      $this.text(data_loading_text);
+      var loading_button = '<a class="load-more-rows-button w-button" href="#">&nbsp;&nbsp;&nbsp;' + data_loading_text + '</a>'
+      $this.html(loading_button);
       $.getScript(more_items_url)
         .done(function(script) {})
         .fail(function() {
