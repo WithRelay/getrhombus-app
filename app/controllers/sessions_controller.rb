794b712123365c70a444244df19ff18951ab0428
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-07-17 17:49:08 -0400

    Messaging Updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < Devise::SessionsController
 
   include AdditionalUserActions
-  before_action :set_user_id, only: [:destroy]
 
   def create
     add_to_merchant_customer_and_referrer_and_fb_cred
@@ -9,15 +8,8 @@ class SessionsController < Devise::SessionsController
   end
 
   def destroy
-    super do |user|
-      $redis_merchant_status.set(@user_id, 'offline')
-    end
-  end
-
-  private
-
-  def set_user_id
-    @user_id = current_user.id.to_s
+    $redis_merchant_status.set(current_user.id.to_s, 'offline')
+    super
   end
 
 end
