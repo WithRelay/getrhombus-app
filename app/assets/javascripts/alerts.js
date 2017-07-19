783ffb8ee2db8dd5a029a4e0d6de3f14c115b58f
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-07-18 23:22:38 -0400

    Alerts/Notifications Updates

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         row: '.form-group',
         validators: {
           callback: {
-            callback:function(value,v alidator, $field){
+            callback:function(value, validator, $field){
               if($('#send-alert').is(':checked') && value == ''){
                 return {
                   valid: false,
