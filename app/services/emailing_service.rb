693be59ac7594608d8965bf03f948909812d20fb
Bhishma Lamichhane <bhishma@getrhombus.com>
2017-07-22 05:57:48 +0545

    Add virtual number parameter on hosted sms email

--- app/services/emailing_service.rb
+++ app/services/emailing_service.rb
@@ -10,11 +10,13 @@ class EmailingService
 
     def send_completed_notice(hosted_number_order)
       user = hosted_number_order.user
+      user.buy_number(user)
       begin
         template_name = 'hosted-sms-activated'
         template_content = []
         message = { "subject" => "Your phone number is activated",
-         "global_merge_vars"=> [  { "name" => "first_name", "content" => user.first_name || 'there' }
+         "global_merge_vars"=> [  { "name" => "first_name", "content" => user.first_name || 'there' },
+                                  { "name" => "virtual_number", "content" => user.rhombus_number }
                                ],
          "merge_language" => "handlebars",
          "to"=> [ { "email" => user.email } ],
@@ -55,12 +57,13 @@ class EmailingService
 
     def hosted_sms_progress_notice(hosted_number_order)
       user = hosted_number_order.user
+      user.buy_number(user)
       begin
         template_name = 'hosted-sms-progress'
         template_content = []
         message = { "subject" => "Status: Complete your phone number activation",
          "global_merge_vars"=> [  { "name" => "first_name", "content" => 'team' },
-                                  { "name" => "virtual_number", "content" => user.rn_friendly_name }
+                                  { "name" => "virtual_number", "content" => user.rhombus_number }
                                ],
          "merge_language" => "handlebars",
          "to"=> [ { "email" => user.email } ],
