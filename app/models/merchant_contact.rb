e4389de1589b98d5207383a9f0eaf5517644d8fb
Bhishma Lamichhane <bhishma@getrhombus.com>
2017-07-25 06:52:59 +0545

    add scope to find fb_page contacts

--- app/models/merchant_contact.rb
+++ app/models/merchant_contact.rb
@@ -2,14 +2,16 @@ class MerchantContact < ActiveRecord::Base
   belongs_to :merchant, class_name: "User"
   has_many :user_lists, as: :customer_contact
   scope :only_contact, -> { where(is_customer: false) }
+  scope :facebook_contacts, -> { where(uid_type: 'fb_page') }
 
   def self.add_or_update_merchant_contact(merchant_id, uid, uid_type)
     begin
       if merchant_id.present? && uid.present?
         # Always update the updated_at field so we know the last time the contact interacted with the merchant
-        find_or_create_by(merchant_id: merchant_id, uid: uid, uid_type: uid_type).touch 
+        find_or_create_by(merchant_id: merchant_id, uid: uid, uid_type: uid_type).touch
       end
     rescue StandardError => err
     end
   end
+
 end
