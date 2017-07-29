b7887c1d283aced6198aaa85e73971ad79fce98f
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-07-29 18:15:22 -0400

    fix list member issue

--- app/controllers/user_lists_controller.rb
+++ app/controllers/user_lists_controller.rb
@@ -5,7 +5,7 @@ class UserListsController < ApplicationController
 		@user_list = UserList.find_by(list_id: params[:list_id], customer_contact_id: params[:member_id], customer_contact_type: cct)
 		@user_list.destroy
     flash[:notice] = "Member has been removed"
-   	redirect_to controller: 'lists', action: 'show', id: params[:list_id]
+   	redirect_to user_list_path(id: params[:list_id], user_id: current_user.id)
 	end
 
 end
