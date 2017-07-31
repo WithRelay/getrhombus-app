50b51a81f64822542a75f4f1f55d32f20855ca6c
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-07-31 10:19:51 -0400

    code review fixes

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -24,31 +24,31 @@ module TransactionsHelper
     tday_txns_count = @all_transactions[0].count
     yday_txns_count = @all_transactions[1].count
     percent_change = (tday_txns_count - yday_txns_count).to_f/yday_txns_count * 100 if yday_txns_count > 0
-    display_change( percent_change.round ) if percent_change.present?
+    display_change(percent_change.round) if percent_change.present?
   end
 
   def transactions_total_amount
-    @tday_txns_amount = 0
-    @all_transactions[0].each{|arr| @tday_txns_amount += arr[0].to_f }
-    @tday_txns_amount
+    @send_btns_disabled = 0
+    @all_transactions[0].each{ |arr| @todays_txns_amount += arr[0].to_f }
+    @todays_txns_amount
   end
 
   def transactions_total_amount_change
     @yday_txns_amount = 0
-    @all_transactions[1].each{|arr| @yday_txns_amount += arr[0] }
-    percent_change = (@tday_txns_amount - @yday_txns_amount).to_f/@yday_txns_amount * 100 if @yday_txns_amount > 0
+    @all_transactions[1].each{ |arr| @yday_txns_amount += arr[0] }
+    percent_change = (@todays_txns_amount - @yday_txns_amount).to_f/@yday_txns_amount * 100 if @yday_txns_amount > 0
     display_change(percent_change.round) if percent_change.present?
   end
 
   def transactions_net_sales
     @tday_net_sale = 0
-    @all_transactions[0].each{|arr| @tday_net_sale +=   (arr[0].to_f - (arr[1].to_f + arr[2].to_f))}
+    @all_transactions[0].each{ |arr| @tday_net_sale += (arr[0].to_f - (arr[1].to_f + arr[2].to_f)) }
     @tday_net_sale
   end
 
   def transactions_net_sales_change
     @yday_net_sale = 0
-    @all_transactions[1].each{|arr| @yday_net_sale += (arr[0] - (arr[1].to_f + arr[2].to_f))}
+    @all_transactions[1].each{ |arr| @yday_net_sale += (arr[0] - (arr[1].to_f + arr[2].to_f)) }
     percent_change = (@tday_net_sale - @yday_net_sale).to_f/@yday_net_sale * 100 if @yday_net_sale > 0
     display_change(percent_change.round) if percent_change.present?
   end
