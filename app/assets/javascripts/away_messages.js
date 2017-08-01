c215a3458acdbb67b390af3d334117e814535c0a
Dorje <torchi@getrhombus.com>
2017-08-01 14:08:49 +0545

    remove checkbox event to show/display text area

--- app/assets/javascripts/away_messages.js
+++ app/assets/javascripts/away_messages.js
@@ -2,8 +2,6 @@ $(document).on('ready page:load', function() {
 
   var autoResponseTextBox = '#Away-Message-Auto-Response'
 
-  $('#enable-away-message').is(':checked') && $('#away-message-text-area').show();
-
   if ($(autoResponseTextBox).length > 0){
     enableEmojiCounter(autoResponseTextBox)
     $(autoResponseTextBox)[0].emojioneArea.on('focus', function(){
@@ -11,13 +9,6 @@ $(document).on('ready page:load', function() {
       $('.emojionearea-editor').counter({ type: 'char', append: false, target: '#away-message-counter' })
     });
   }
-  $('#enable-away-message').click(function(){
-    if($(this).is(":checked")){
-      $('#away-message-text-area').slideDown();
-    }else{
-      $('#away-message-text-area').hide();
-    }
-  });
 
   $('#save-away-message-button').click(function(e){
     if ($('#enable-away-message').is(':checked') && $(autoResponseTextBox).val() == ''){
