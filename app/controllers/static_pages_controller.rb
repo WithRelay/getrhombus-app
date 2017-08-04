c96cf541ecd83632bc82f80be6dbc8f9183f3661
Dorje <torchi@getrhombus.com>
2017-08-04 11:24:52 +0545

    update action name

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,13 +6,13 @@ class StaticPagesController < ApplicationController
     end
   end
 
-  [:use_case_education, :use_case_non_profit, :use_case_demand_service, :use_case_sales_marketing, :use_case_staffing_employment].each do |method_name|
+  [:text_messaging_for_schools_and_universities, :text_messaging_for_donations_and_nonprofits, :text_messaging_for_on_demand_services, :text_messaging_for_sales_and_marketing, :text_messaging_for_staffing_agencies].each do |method_name|
     send :define_method, method_name do
       # do nothing
     end
   end
 
-  [:refer_a_business, :relay_docs, :platform_integrations, :use_case_customer_support].each do |method_name|
+  [:refer_a_business, :relay_docs, :platform_integrations, :text_messaging_for_customer_support].each do |method_name|
     send :define_method, method_name do
       # do nothing
     end
