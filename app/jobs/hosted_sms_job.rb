24ef51404e275a0783dd7daeaeceb3f2e9dc5956
Bhishma Lamichhane <bhishma@getrhombus.com>
2017-08-07 08:45:32 +0545

    release and save rhombus number

--- app/jobs/hosted_sms_job.rb
+++ app/jobs/hosted_sms_job.rb
@@ -7,22 +7,24 @@ class HostedSmsJob
 
     HostedSms.includes(:user).not_completed.each do |h|
       HostedSmsService.get_status(h)
-      if h.status.downcase == 'pending-loa' && !h.status_events[:loa_sent]
+      if h.status.casecmp('pending-loa').zero? && !h.status_events[:loa_sent]
         HostedSmsService.request_loa(h)
         h.status_events[:loa_sent] = true
         h.status_events[:loa_sent_at] = Time.now
         h.save
-      elsif h.status.downcase == 'completed' && !h.status_events[:completed_notice_sent]
+      elsif h.status.casecmp('completed').zero? && !h.status_events[:completed_notice_sent]
+        break unless TextingService.release_number(h.user.rhombus_number) || h.user.rhombus_number.nil?
         EmailingService.send_completed_notice(h.user)
+        h.user.update_rhombus_number(h.phone_number)
         h.status_events[:completed_notice_sent] = true
         h.status_events[:completed_notice_sent_at] = Time.now
         h.save
-      elsif h.status.downcase == 'action-required' && !h.status_events[:action_required_notice_sent]
+      elsif h.status.casecmp('action-required').zero? && !h.status_events[:action_required_notice_sent]
         EmailingService.send_action_required_notice(h.user)
         h.status_events[:action_required_notice_sent] = true
         h.status_events[:action_required_notice_sent_at] = Time.now
         h.save
-      elsif h.status.downcase == 'failed' && !h.status_events[:failed_notice_sent]
+      elsif h.status.casecmp('failed').zero? && !h.status_events[:failed_notice_sent]
         EmailingService.send_failed_notice(h.user)
         h.status_events[:failed_notice_sent] = true
         h.status_events[:failed_notice_sent_at] = Time.now
@@ -30,5 +32,4 @@ class HostedSmsJob
       end
     end
   end
-
 end
