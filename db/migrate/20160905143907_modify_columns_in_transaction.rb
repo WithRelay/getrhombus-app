241729a7a4eb924e84bc19468ede8638e18194ce
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-09-19 08:53:06 -0400

    transaction number migration index issue

--- db/migrate/20160905143907_modify_columns_in_transaction.rb
+++ db/migrate/20160905143907_modify_columns_in_transaction.rb
@@ -3,7 +3,7 @@ class ModifyColumnsInTransaction < ActiveRecord::Migration
     rename_column :transactions, :expiration_month, :exp_month
     rename_column :transactions, :expiration_year, :exp_year
     rename_column :transactions, :transaction_uri, :txn_uri
-    #rename_index :people, 'index_transactions_on_transaction_number', 'index_transactions_on_txn_number'
+    rename_index :transactions, 'index_transactions_on_transaction_number', 'index_transactions_on_txn_number'
     rename_column :transactions, :transaction_number, :txn_number
     rename_column :transactions, :transaction_available_at, :txn_available_at
     change_column :transactions, :amount_with_taxes, :decimal, precision: 8, scale: 2, after: :amount
