ee4c98b116e82b38c1139425b8a3bf63f50bd197
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-10-05 11:30:19 -0400

    change default url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.action_mailer.default_url_options = { host: 'https://test.withrelay.com', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://test.withrelay.com' }
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
