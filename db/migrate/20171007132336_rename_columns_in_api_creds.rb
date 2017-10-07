55c4947348351eab5fed15689541700fb8de968b
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-10-07 10:50:26 -0400

    uncomment migration

--- db/migrate/20171007132336_rename_columns_in_api_creds.rb
+++ db/migrate/20171007132336_rename_columns_in_api_creds.rb
@@ -3,7 +3,7 @@ class RenameColumnsInApiCreds < ActiveRecord::Migration
     remove_index :api_creds, name: :index_api_creds_on_api_secret
     remove_index :api_creds, name: :index_api_creds_on_api_key
 
-    #rename_column :api_creds, :api_secret, :secret
+    rename_column :api_creds, :api_secret, :secret
     rename_column :api_creds, :api_key, :key
 
     add_index(:api_creds, :key, unique: true)
