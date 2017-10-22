93492b6272e098aa64a7a9c9c763ed7676310268
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-10-21 21:25:08 -0400

    csv upload updates

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,25 @@ class Document < ActiveRecord::Base
 
   belongs_to :user
   has_attached_file :attachment
-  validates_attachment :attachment, content_type: { content_type: ["text/csv", 'application/vnd.ms-excel'], message: "Please save file as a CSV." }
+  validates_attachment_content_type :attachment, content_type: ["text/csv", 'application/vnd.ms-excel']
+  
+  #Attachment content type Please save file as a CSV.
+  #Attachment Please save file as a CSV.
 
+  after_validation :clean_up_paperclip_errors
 
+  private
+
+  # This is because paperclip duplicates error messages
+  # See: https://github.com/thoughtbot/paperclip/pull/1554 and
+  # https://github.com/thoughtbot/paperclip/commit/2aeb491fa79df886a39c35911603fad053a201c0
+  def clean_up_paperclip_errors
+    errors.delete(:attachment)
+    if errors[:attachment_content_type].present?
+      errors.add(:base, 'Please save file as a CSV and re-upload') 
+      errors.delete(:attachment_content_type)
+    end
+  end
+
+  
 end
