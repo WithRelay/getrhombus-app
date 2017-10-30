2088921b49899a0a9b7dba12f4e0881904485ac9
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-10-29 22:32:14 -0400

    fix migration

--- db/migrate/20170517173354_remove_foreign_key_constraints.rb
+++ db/migrate/20170517173354_remove_foreign_key_constraints.rb
@@ -1,19 +1,19 @@
 class RemoveForeignKeyConstraints < ActiveRecord::Migration
   def change   
     foreign_keys(:alerts).each do |k|
-      remove_foreign_key :alerts, name: :k
+      remove_foreign_key :alerts, name: k
     end
 
     foreign_keys(:invoices).each do |k|
-      remove_foreign_key :invoices, name: :k
+      remove_foreign_key :invoices, name: k
     end
 
     foreign_keys(:subscriptions).each do |k|
-      remove_foreign_key :subscriptions, name: :k
+      remove_foreign_key :subscriptions, name: k
     end
 
     foreign_keys(:transactions).each do |k|
-      remove_foreign_key :transactions, name: :k
+      remove_foreign_key :transactions, name: k
     end
   end
 end
