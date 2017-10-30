8ae419c9f0650b3a2e1f7b6920caed8553551313
Taiwo Oyeniyi <taiwo@getrhombus.com>
2017-10-29 20:28:47 -0400

    check if table or column exists in migration

--- db/migrate/20161222045450_add_channel_id_to_notification_logs.rb
+++ db/migrate/20161222045450_add_channel_id_to_notification_logs.rb
@@ -1,6 +1,6 @@
 class AddChannelIdToNotificationLogs < ActiveRecord::Migration
   def change
-    add_column :notification_logs, :channel_id, :integer, after: :channel
-    add_index :notification_logs, :channel_id
+    add_column :notification_logs, :channel_id, :integer, after: :channel unless column_exists? :notification_logs, :channel_id
+    add_index :notification_logs, :channel_id unless index_exists? :notification_logs, :channel_id
   end
 end
