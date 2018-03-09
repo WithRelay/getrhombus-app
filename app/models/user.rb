6c15cf6d2711a3c9ef83b63a6e9955fffbb623ec
Taiwo Oyeniyi <taiwo@getrhombus.com>
2018-03-09 02:17:35 -0500

    RHOMBUSV1-225

--- app/models/user.rb
+++ app/models/user.rb
@@ -141,16 +141,14 @@ class User < ActiveRecord::Base
   def self.get_platform_acct_obj; User.find_by(email: User.platform_email) end
   def deduct_from_account_balance(amt); self.decrement!(:account_balance, amt.to_f) end
 
-
-  ##### change
-  def friendly_relay_number; self.rn_friendly_name.present? ? self.rn_friendly_name : self.rhombus_number end
+  #def friendly_relay_number; self.rn_friendly_name.present? ? self.rn_friendly_name : self.rhombus_number end
   # A default number must always exists for active accounts
-  #def rhombus_number; self.default_number.try(:number) end   # uncomment after removing columns
-  #def friendly_relay_number
-   # dn = self.default_number
-   # dn.friendly_name.present? ? dn.friendly_name : dn.number 
-  #end
-  
+  def rhombus_number; self.default_number.try(:number) end   # uncomment after removing columns
+  def friendly_relay_number
+    dn = self.default_number
+    dn.friendly_name.present? ? dn.friendly_name : dn.number 
+  end
+
   def managed_account_is_verified?; stripe_creds.first.try(:legal_entity_verification).try(:[], 'status') == 'verified' end
 
   def full_name
@@ -206,7 +204,6 @@ class User < ActiveRecord::Base
     uid = generate_uid
     url = "#{url_helpers.new_user_registration_url}?referrer_uid=#{uid}"
 
-    #### change
     #self.update(relay_uid: uid, rhombus_number: number[0], rn_friendly_name: number[1], short_url: url, rn_type: params["rn_type"], rn_country: params["rn_country"])
     self.update(relay_uid: uid, short_url: url)
     self.numbers.create(number: number[0], friendly_name: number[1], number_type: params["rn_type"], country: params["rn_country"], default: true)
@@ -254,7 +251,6 @@ class User < ActiveRecord::Base
 
   private
 
-  #### Change
   # Some users sign up with Relay numbers
   def phone_number_cannot_be_rhombus_number
     #if self.phone_number.present? && User.exists?(rhombus_number: self.phone_number)
