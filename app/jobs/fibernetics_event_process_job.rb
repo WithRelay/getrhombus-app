ace420e9dab7861692fb2142afac12dad92ed124
Bhishma Lamichhane <bhishma@getrhombus.com>
2018-04-26 06:01:10 +0545

    RHOMBUSV1-230 add queue

--- app/jobs/fibernetics_event_process_job.rb
+++ app/jobs/fibernetics_event_process_job.rb
@@ -1,5 +1,7 @@
 # background job to process fibernetics event
 class FiberneticsEventProcessJob < ApplicationJob
+  @queue = Rails.env + "_fibernetics_event"
+
   def perform(params)
     FiberneticsEvent.new.process_event(params)
   end
