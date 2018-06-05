d40f7a66e1db33aaceff145a90f2e4cdc37ab1a5
Taiwo Oyeniyi <taiwo@withrelay.com>
2018-06-05 18:02:51 -0400

    comment out removing index

--- db/migrate/20180605212722_remove_indexes_on_to_in_messages.rb
+++ db/migrate/20180605212722_remove_indexes_on_to_in_messages.rb
@@ -1,6 +1,6 @@
 class RemoveIndexesOnToInMessages < ActiveRecord::Migration
   def change
-  	remove_index :messages, :from
-  	remove_index :messages, :to
+  	#remove_index :messages, :from
+  	#remove_index :messages, :to
   end
 end
