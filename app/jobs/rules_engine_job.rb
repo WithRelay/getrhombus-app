005dfa5392524da32fa56c6cece377a45236b07f
taiwo_ <taiwo@withrelay.com>
2020-06-26 17:50:58 -0400

    Rules updates

--- app/jobs/rules_engine_job.rb
+++ app/jobs/rules_engine_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RulesEngineJob < ApplicationJob
   queue_as :rules
 
@@ -7,69 +9,65 @@ class RulesEngineJob < ApplicationJob
   end
 
   def run_rules
-    begin
-      message_text = @message.text.downcase.strip
-      return if message_text.blank?
+    message_text = @message.text.downcase.strip
+    return if message_text.blank?
 
-      @merchant = User.find_by(id: @message.user_id_to)
-      rules = @merchant.rules
-      return if rules.blank?
+    @merchant = User.find_by(id: @message.user_id_to)
+    rules = @merchant.rules.pluck(:text, :response, :rule_type, :message_length, :id)
+    return if rules.blank?
 
-      rules.each do |rule|
-        rule_text = rule.text.downcase.strip
-        response = rule.response
+    rules.each do |rule|
+      rule_text = rule.first.downcase.strip
+      response = rule.second
 
-        case rule.rule_type
-        when 'starts_with_text'
-          if message_text.starts_with?(rule_text)
-            send_response(response)
-            break
-          end
-        when 'starts_with_text_and_length_is_less_than_x'
-          if message_text.starts_with?(rule_text) && message_text.size < (rule.message_length + 1)
-            send_response(response)
-            break
-          end
-        when 'contains_text'
-          if message_text.include?(rule_text)
-            send_response(response)
-            break
-          end
-        when 'contains_only_text'
-          if message_text == rule_text
-            send_response(response)
-            break
-          end
-        when 'contains_text_and_length_is_less_than_x'
-          if message_text.include?(rule_text) && message_text.size  < (rule.message_length + 1)
-            send_response(response)
-            break
-          end
-        else
-          ExceptionNotifier.notify_exception(
-            StandardError.new,
-            env: Rails.env,
-            data: {
-              message: 'From RulesEngineJob, rule mismatch',
-              rule: rule.attributes,
-              env: Rails.env
-            }
-          )
+      case rule.third
+      when 'starts_with_text'
+        if message_text.starts_with?(rule_text)
+          send_response(response)
+          break
         end
+      when 'starts_with_text_and_length_is_less_than_x'
+        if message_text.starts_with?(rule_text) && message_text.size < (rule.fourth + 1)
+          send_response(response)
+          break
+        end
+      when 'contains_text'
+        if message_text.include?(rule_text)
+          send_response(response)
+          break
+        end
+      when 'contains_only_text'
+        if message_text == rule_text
+          send_response(response)
+          break
+        end
+      when 'contains_text_and_length_is_less_than_x'
+        if message_text.include?(rule_text) && message_text.size < (rule.fourth + 1)
+          send_response(response)
+          break
+        end
+      else
+        ExceptionNotifier.notify_exception(
+          StandardError.new,
+          env: Rails.env,
+          data: {
+            message: 'From RulesEngineJob, rule mismatch',
+            rule: rule,
+            env: Rails.env
+          }
+        )
       end
-
-    rescue Exception => err
-      ExceptionNotifier.notify_exception(
-        StandardError.new,
-        env: Rails.env,
-        data: {
-          message: 'From RulesEngineJob, something went wrong',
-          message: @message,
-          env: Rails.env
-        }
-      )
     end
-
+  rescue Exception => e
+    ExceptionNotifier.notify_exception(
+      StandardError.new,
+      env: Rails.env,
+      data: {
+        message: 'From RulesEngineJob, something went wrong',
+        message: @message,
+        env: Rails.env
+      }
+    )
   end
 
   def send_response(response_text)
@@ -84,7 +82,9 @@ class RulesEngineJob < ApplicationJob
       uid_type = 'phone_number'
       person = MerchantContact.find_by(merchant_id: @merchant.id, uid: uid, uid_type: uid_type)
     end
-    response_text = CampaignHandlebar.new(person, @merchant).render(response_text) if person.present? && @merchant.present?
+    if person.present? && @merchant.present?
+      response_text = CampaignHandlebar.new(person, @merchant).render(response_text)
+    end
     Conversation.find_or_create_conversation_for_message_and_send_publish(@merchant, customer, uid_type, uid, response_text, 'Message', [], 'platform', @message.to)
   end
 end
