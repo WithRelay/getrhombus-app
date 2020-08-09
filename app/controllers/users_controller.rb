306e83efcb3bf41c67bc19796172486ecde8d410
taiwo_ <taiwo@withrelay.com>
2020-08-09 19:45:45 -0400

    block numbers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
-class UsersController < ApplicationController
+# frozen_string_literal: true
 
+class UsersController < ApplicationController
   before_action :set_user
   load_and_authorize_resource
-  
+
   include DashboardNotification
   include AdditionalUserActions
   include ManagedAccountActions
   include DashboardData
 
-  before_action :set_notifications, except: [:show, :customer_csv_template, :contact_csv_template, :add_card_info, :add_subscription, :add_rhombus_number,
-                                             :add_profile_info, :remove_twitter_integration, :remove_stripe_integration, :verify_hosted_sms_order]
+  before_action :set_notifications, except: %i[show customer_csv_template contact_csv_template add_card_info add_subscription add_rhombus_number
+                                               add_profile_info remove_twitter_integration remove_stripe_integration verify_hosted_sms_order]
 
   def show
     if current_user.is_merchant?
@@ -19,7 +20,7 @@ class UsersController < ApplicationController
       @dashboard_msg_perform = dashboard_analytics_section
       @dashboard_transactions = dashboard_transactions
       @dashboard_messages_data = dashboard_messages_data
-    else 
+    else
       redirect_to user_transactions_path
     end
   end
@@ -33,7 +34,8 @@ class UsersController < ApplicationController
     end
   end
 
-private
+  private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = current_user
@@ -42,18 +44,16 @@ private
   def full_user_params
     nested_user_params = user_params
     tos_params = { ip: request.remote_ip, user_agent: request.user_agent, tos_date: Time.current }
-    nested_user_params[:stripe_creds_attributes]["0"].merge!(tos_params)
+    nested_user_params[:stripe_creds_attributes]['0'].merge!(tos_params)
     nested_user_params
   end
 
   def user_params
     params.require(:user).permit(:id, :org_type, :org_name, :url, :org_tax_id, :description, :tos_acceptance,
-      bank_accounts_attributes: [:id, :routing_number, :country, :currency, :account_number, :institution_number],
-      people_attributes: [:id, :gender, :business_name, :full_name, :dob, :last4, :role, :_destroy, 
-        #address_attributes: [:street_address, :suite, :id, :city, :state_province, :postal_code, :country]
-      ],
-      address_attributes: [:street_address, :suite, :id, :city, :state_province, :postal_code, :country],
-      stripe_creds_attributes: [:id, :charges_enabled, :payouts_enabled])
+                                 bank_accounts_attributes: %i[id routing_number country currency account_number institution_number],
+                                 people_attributes: [:id, :gender, :business_name, :full_name, :dob, :last4, :role, :_destroy]
+                                   # address_attributes: [:street_address, :suite, :id, :city, :state_province, :postal_code, :country],
+                                 address_attributes: %i[street_address suite id city state_province postal_code country],
+                                 stripe_creds_attributes: %i[id charges_enabled payouts_enabled])
   end
-
 end
