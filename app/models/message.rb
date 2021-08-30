6ad3c1f584f0e95d0a64b007f1e8bdc5e0e00caa
taiwo_ <taiwo@withrelay.com>
2021-08-30 15:13:15 -0400

    updates

--- app/models/message.rb
+++ app/models/message.rb
@@ -1339,29 +1339,9 @@ class Message < ActiveRecord::Base
   end
 
   def wq3
-    rules = Rule.where(user_id: 48_897).pluck(:text, :rule_type, :response, :message_length)
+    rules = Rule.where(user_id: 220_257).pluck(:text, :rule_type, :response, :message_length)
 
-    %w[<redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>
-       <redacted_email>].each do |e|
+    %w[<redacted_email>].each do |e|
       user = User.find_by(email: e.downcase)
 
       data = []
